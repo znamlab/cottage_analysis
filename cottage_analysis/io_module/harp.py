568ac4058be31393733ad576737f6d4e2330e0e0
Antonin Blot <antonin.blot@gmail.com>
2021-04-16 17:19:16 +0100

    Minor fix and remove useless read_harp in __init__.py

--- cottage_analysis/io_module/harp.py
+++ cottage_analysis/io_module/harp.py
@@ -13,28 +13,28 @@ PAYLOAD_TYPE = {0: 'isUnsigned', 128: 'isSigned', 64: 'isFloat', 16: 'Timestamp'
                 130: 'S16', 4: 'U32', 132: 'S32', 8: 'U64', 136: 'S64', 68: 'Float', 17: 'TimestampedU8',
                 145: 'TimestampedS8', 18: 'TimestampedU16', 146: 'TimestampedS16', 20: 'TimestampedU32',
                 148: 'TimestampedS32', 24: 'TimestampedU64', 152: 'TimestampedS64', 84: 'TimestampedFloat'}
-PAYLOAD_STRUCT = {1: 'B',  # - T U8 : Unsigned 8 bits
-                  2: 'H',  # T U16 : Unsigned 16 bits
-                  4: 'I',  # T U32 : Unsigned 32 bits
-                  8: 'Q',  # T U32 : Unsigned 64 bits
-                  129: 'b',  # T I8 : Signed 8 bits
-                  130: 'h',  # T I16 : Signed 16 bits
-                  132: 'i',  # T I32 : Signed 32 bits
-                  136: 'l',  # T I64 : Signed 64 bits
-                  68: 'f',  # T Float : Single-precision floating-point 32 bits
-                  16: 'IH',  # Timestamped<> : Time information only
-                  17: 'IHB',  # Timestamped<T> U8 : Timestamped unsigned 8 bits
-                  18: 'IHH',  # Timestamped<T> U16 : Timestamped unsigned 16 bits
-                  20: 'IHI',  # Timestamped<T> U32 : Timestamped unsigned 32 bits
-                  24: 'IHQ',  # Timestamped<T> U64 : Timestamped unsigned 64 bits
-                  145: 'IHb',  # Timestamped<T> I8 : Timestamped signed 8 bits
-                  146: 'IHh',  # Timestamped<T> I16 : Timestamped signed 16 bits
-                  148: 'IHi',  # Timestamped<T> I32 : Timestamped signed 32 bits
-                  152: 'IHl',  # Timestamped<T> I64 : Timestamped signed 64 bits
-                  84: 'IHf'  # Timestamped<T> Float : Timestamped Single-precision floating-point 32 bits
-                  }
+_PAYLOAD_STRUCT = {1: 'B',  # - T U8 : Unsigned 8 bits
+                   2: 'H',  # T U16 : Unsigned 16 bits
+                   4: 'I',  # T U32 : Unsigned 32 bits
+                   8: 'Q',  # T U32 : Unsigned 64 bits
+                   129: 'b',  # T I8 : Signed 8 bits
+                   130: 'h',  # T I16 : Signed 16 bits
+                   132: 'i',  # T I32 : Signed 32 bits
+                   136: 'l',  # T I64 : Signed 64 bits
+                   68: 'f',  # T Float : Single-precision floating-point 32 bits
+                   16: 'IH',  # Timestamped<> : Time information only
+                   17: 'IHB',  # Timestamped<T> U8 : Timestamped unsigned 8 bits
+                   18: 'IHH',  # Timestamped<T> U16 : Timestamped unsigned 16 bits
+                   20: 'IHI',  # Timestamped<T> U32 : Timestamped unsigned 32 bits
+                   24: 'IHQ',  # Timestamped<T> U64 : Timestamped unsigned 64 bits
+                   145: 'IHb',  # Timestamped<T> I8 : Timestamped signed 8 bits
+                   146: 'IHh',  # Timestamped<T> I16 : Timestamped signed 16 bits
+                   148: 'IHi',  # Timestamped<T> I32 : Timestamped signed 32 bits
+                   152: 'IHl',  # Timestamped<T> I64 : Timestamped signed 64 bits
+                   84: 'IHf'  # Timestamped<T> Float : Timestamped Single-precision floating-point 32 bits
+                   }
 # specify endianess explicitly
-PAYLOAD_STRUCT = {k: '<' + v for k, v in PAYLOAD_STRUCT.items()}
+_PAYLOAD_STRUCT = {k: '<' + v for k, v in _PAYLOAD_STRUCT.items()}
 
 
 def read_message(path_to_file, verbose=True, valid_addresses=None, valid_msg_type=None):
@@ -110,7 +110,7 @@ def read_message(path_to_file, verbose=True, valid_addresses=None, valid_msg_typ
 
             msg_end = binary_file.read(length - 3)  # ignore the fields I have already read
             # find how many data element I have
-            payload_struct = PAYLOAD_STRUCT[payload_type]
+            payload_struct = _PAYLOAD_STRUCT[payload_type]
             data_size = struct.calcsize(payload_struct[-1])
             offset = 6 if PAYLOAD_TYPE[payload_type].startswith('Timestamp') else 0
             num_elements = (len(msg_end[:-1]) - offset) / data_size
@@ -149,3 +149,4 @@ def read_message(path_to_file, verbose=True, valid_addresses=None, valid_msg_typ
             erase_line = ('\b' * len(text_msg))
             print(erase_line + "Packing into dataframe...")
         return pd.DataFrame(all_msgs)
+
