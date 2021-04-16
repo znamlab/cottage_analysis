4769d7ca24ea9e625fd71933f6f2a3f6a8f70f9d
Antonin Blot <antonin.blot@gmail.com>
2021-04-16 12:08:42 +0100

    Fix test_training
    Make sure output_dir exists

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,18 +1,16 @@
 from pathlib import Path
 import os
-
-ROOT_DIR = Path("tests", "test_data")
 from cottage_analysis.eye_tracking import create_training_dataset
 
-
+ROOT_DIR = Path("tests", "test_data")
 OUT_DIR = os.path.join(ROOT_DIR, 'tests_outputs')
-MOUSE = "PZAH3.1c"
-SESSION = "S20210406"
-RECORDING = "R174548"
-EXAMPLE_HARP = "harp_messages_example.bin"
+if not os.path.isdir(OUT_DIR):
+    os.mkdir(OUT_DIR)
+
 
 for file_name in os.listdir(OUT_DIR):
     os.remove(os.path.join(OUT_DIR, file_name))
+
 
 def test_subsample():
     create_training_dataset.generate_subset(input_dir=ROOT_DIR, output_dir=OUT_DIR, camera='right_eye_camera')
