3af88c3e8ac157121d3bd465d50dc54f87a4051b
Yiran He <yiran.he@crick.ac.uk>
2021-05-17 19:37:47 +0100

    Update data transpose

--- cottage_analysis/io_module/video/transform.py
+++ cottage_analysis/io_module/video/transform.py
@@ -9,6 +9,36 @@ Transform the shape of data
 """
 import numpy as np
 
+
+def find_idx_colMajor(idx, R, C, F):
+    '''
+    Convert the index of data saved in F-order (Bonsai) to C-order coordinate
+
+    Parameters
+    ----------
+    idx : int
+        F-order index of data
+    R : int
+        Total number of rows
+    C : int
+        Total number of columns
+    F : int
+        Total number of frames
+
+    Returns
+    -------
+    [r.c.f]: [row_idx, col_idx, frame_idx] in C-order
+
+    '''
+    
+    f = int(idx/(R*C))
+    c = int((idx-f*(R*C))/R)
+    r = (idx-R*C*f) - c*R
+    
+    return[r,c,f]
+
+
+
 def colMajor_to_frameMajor(idx, R, C, F):
     '''
     Convert the index of data saved in F-order (Bonsai) to the index of frame-major data to be saved
@@ -30,9 +60,7 @@ def colMajor_to_frameMajor(idx, R, C, F):
         Index of frame-major data to be saved 
 
     '''
-    f = int(idx/(R*C))
-    c = int((idx-f*(R*C))/R)
-    r = (idx-R*C*f) - c*R
+    [r,c,f] = find_idx_colMajor(idx, R, C, F)
     
     new_idx = (r*C+c)*F + f
     
