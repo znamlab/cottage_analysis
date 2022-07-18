e2d539b64c380b9346f90313e1fb7b585806c81b
yiran25 <yiran.he@crick.ac.uk>
2022-07-18 18:53:24 +0100

    Change setup.py to find all packages.

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name='cottage_analysis',
     version='v0.1',
-    packages=['cottage_analysis', 'cottage_analysis.io_module', 'cottage_analysis.io_module.video',
-              'cottage_analysis.eye_tracking'],
+    packages=find_packages(),
     url='https://github.com/znamlab/cottage_analysis',
     license='MIT',
     author='Antonin Blot, Yiran He, Petr Znamenskyi',
