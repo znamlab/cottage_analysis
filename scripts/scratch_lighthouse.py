88c0d32bbfbd8d37a2be038f43b93bd601d4b36c
Antonio Colas Nieto <anconi.1999@gmail.com>
2022-08-11 01:51:40 +0100

    Finished the affine transformatioon, wrote a couple of functions to make it work on every point, plotted every point, started work on the 2D histograms

--- scripts/scratch_lighthouse.py
+++ scripts/scratch_lighthouse.py
@@ -126,6 +126,20 @@ plt.xlabel("X axis")
 plt.ylabel("Y axis")
 plt.colorbar()
 
+#2D histogram technique
+
+plt.close('all')
+
+plt.figure(figsize=(6.8, 4.2))
+complete_hist = plt.hexbin(data_1.iloc[:,2], data_1.iloc[:,3])
+plt.figure(figsize=(6.8, 4.2))
+delayed_hist = plt.hexbin(data_1_delayed.iloc[:,2], data_1_delayed.iloc[:,3])
+
+complete_hist = complete_hist.get_array
+
+
+###################################################################
+
 #Objective accomplished, but so far, no relevant insights, perhaps divide in a tesselation and calculate average delay?
 
 #We first read the six csvs and create the two relevant matrices: the ARUCO one and the lighthouse one
@@ -135,47 +149,133 @@ plt.close('all')
 calibration_directory = "/Users/antoniocolas/code/cottage_rawdata/lighthouse_calibration/"
 calibration_session = "S20220808"
 
-data_ref = pd.read_csv(calibration_directory+calibration_session+"/calibration_reference/ts4231-1_2022-08-08T16_31_57.csv")
-data_x = pd.read_csv(calibration_directory+calibration_session+"/calibration_xaxis/ts4231-1_2022-08-08T16_33_05.csv")
-data_y = pd.read_csv(calibration_directory+calibration_session+"/calibration_yaxis/ts4231-1_2022-08-08T16_33_54.csv")
+data_ref = pd.read_csv(calibration_directory+calibration_session+"/calibration_reference/ts4231-2_2022-08-08T16_31_57.csv")
+data_x = pd.read_csv(calibration_directory+calibration_session+"/calibration_xaxis/ts4231-2_2022-08-08T16_33_05.csv")
+data_y = pd.read_csv(calibration_directory+calibration_session+"/calibration_yaxis/ts4231-2_2022-08-08T16_33_54.csv")
 
-light_posit_ref = [stats.mean(data_ref.iloc[:,2]), stats.mean(data_ref.iloc[:,3]), stats.mean(data_ref.iloc[:,4])]
-light_posit_x = [stats.mean(data_x.iloc[:,2]), stats.mean(data_x.iloc[:,3]), stats.mean(data_x.iloc[:,4])]
-light_posit_y = [stats.mean(data_y.iloc[:,2]), stats.mean(data_y.iloc[:,3]), stats.mean(data_y.iloc[:,4])]
-light_posit_ortho = [0]*3
+light_posit_ref = np.array([stats.mean(data_ref.iloc[:,2]), stats.mean(data_ref.iloc[:,3]), stats.mean(data_ref.iloc[:,4]), 1])
+light_posit_x = np.array([stats.mean(data_x.iloc[:,2]), stats.mean(data_x.iloc[:,3]), stats.mean(data_x.iloc[:,4]), 1])
+light_posit_y = np.array([stats.mean(data_y.iloc[:,2]), stats.mean(data_y.iloc[:,3]), stats.mean(data_y.iloc[:,4]), 1])
 
-for dimension in [1,2,3]:
-    if dimension+1 == 4:
-        index_1 = 1
-    else:
-        index_1 = dimension+1
-    if dimension-1 == -1:
-        index_2 = 3
-    else:
-        index_2 = dimension-1
-    light_posit_ortho[dimension-1] = (light_posit_x[index_1-1]*light_posit_y[index_2-1])-(light_posit_x[index_2-1]*light_posit_y[index_2-1])
 
-aruc_posit_ref = [0, 0, 0]
-aruc_posit_x = [30, 0, 0]
-aruc_posit_y = [0, 30, 0]
-aruc_posit_ortho = [0, 0, 30]
+light_posit_refx = light_posit_x[0:3]-light_posit_ref[0:3]
+light_posit_refy = light_posit_y[0:3]-light_posit_ref[0:3]
 
-aruc_matrix = np.transpose(np.array([aruc_posit_x, aruc_posit_y, aruc_posit_ortho]))
-light_matrix = np.transpose(np.array([light_posit_x, light_posit_y, light_posit_ortho]))
+light_posit_refortho = np.cross(light_posit_refy, light_posit_refx)
+light_posit_ortho = light_posit_ref[0:3]+light_posit_refortho
+
+light_posit_refx, light_posit_refy, light_posit_refortho, light_posit_ortho = list(map(np.append, [light_posit_refx, light_posit_refy, light_posit_refortho, light_posit_ortho], [1]*4))
+
+#The orthogonal vectors point upwards in the arena, I think it makes sense, check with Antonin
+
+aruc_posit_ref = np.array([0, 0, 0, 1])
+aruc_posit_x = np.array([30, 0, 0, 1])
+aruc_posit_y = np.array([0, 30, 0, 1])
+aruc_posit_ortho = np.array([0, 0, 30, 1])
+
+aruc_matrix = np.transpose([aruc_posit_ref, aruc_posit_x, aruc_posit_y, aruc_posit_ortho])
+light_matrix = np.transpose([light_posit_ref, light_posit_x, light_posit_y, light_posit_ortho])
 
 #The result is obtained by multiplying the desired set by the inverted original set
 
+transform_matrix = np.matmul(aruc_matrix, np.linalg.inv(light_matrix))
+
+#When you multiply a lighthouse position vector by the transform matrix you should get the aruco coordinates
+
+#Let's test that.
+
+np.matmul(transform_matrix, np.transpose(light_posit_x))
+
+#Now we transform every point of the recording and repeat the plots.
+
+calibration_directory = "/Users/antoniocolas/code/cottage_rawdata/lighthouse_calibration/"
+calibration_session = "S20220808"
+
+reference, x_axis, y_axis = ["/calibration_reference/ts4231-2_2022-08-08T16_31_57.csv", "/calibration_xaxis/ts4231-2_2022-08-08T16_33_05.csv", "/calibration_yaxis/ts4231-2_2022-08-08T16_33_54.csv"]
 
 
-#light_vect_refx = [0]*3
-#light_vect_refy = [0]*3
-#light_vect_ortho = [0]*3
+def obtain_transform_matrix(calibration_directory, calibration_session, x_axis, y_axis, reference):
 
-#for dimension in [0, 1, 2]:
-    #light_vect_refx = light_posit_x[dimension]-light_posit_ref[dimension]
-    #light_vect_refy = light_posit_y[dimension]-light_posit_ref[dimension]
-   # light_vect_ortho = light_vect
+    data_ref = pd.read_csv(
+        calibration_directory + calibration_session + reference)
+    data_x = pd.read_csv(
+        calibration_directory + calibration_session + y_axis)
+    data_y = pd.read_csv(
+        calibration_directory + calibration_session + x_axis)
+
+    light_posit_ref = np.array(
+        [stats.mean(data_ref.iloc[:, 2]), stats.mean(data_ref.iloc[:, 3]), stats.mean(data_ref.iloc[:, 4]), 1])
+    light_posit_x = np.array(
+        [stats.mean(data_x.iloc[:, 2]), stats.mean(data_x.iloc[:, 3]), stats.mean(data_x.iloc[:, 4]), 1])
+    light_posit_y = np.array(
+        [stats.mean(data_y.iloc[:, 2]), stats.mean(data_y.iloc[:, 3]), stats.mean(data_y.iloc[:, 4]), 1])
+
+    light_posit_refx = light_posit_x[0:3] - light_posit_ref[0:3]
+    light_posit_refy = light_posit_y[0:3] - light_posit_ref[0:3]
+
+    light_posit_refortho = np.cross(light_posit_refy, light_posit_refx)
+    light_posit_ortho = light_posit_ref[0:3] + light_posit_refortho
+
+    light_posit_refx, light_posit_refy, light_posit_refortho, light_posit_ortho = list(
+        map(np.append, [light_posit_refx, light_posit_refy, light_posit_refortho, light_posit_ortho], [1] * 4))
+
+    # The orthogonal vectors point upwards in the arena, I think it makes sense, check with Antonin
+
+    aruc_posit_ref = np.array([0, 0, 0, 1])
+    aruc_posit_x = np.array([30, 0, 0, 1])
+    aruc_posit_y = np.array([0, 30, 0, 1])
+    aruc_posit_ortho = np.array([0, 0, 30, 1])
+
+    aruc_matrix = np.transpose([aruc_posit_ref, aruc_posit_x, aruc_posit_y, aruc_posit_ortho])
+    light_matrix = np.transpose([light_posit_ref, light_posit_x, light_posit_y, light_posit_ortho])
+
+    # The result is obtained by multiplying the desired set by the inverted original set
+
+    transform_matrix = np.matmul(aruc_matrix, np.linalg.inv(light_matrix))
+    return transform_matrix
+
+def transform_data(data, transform_matrix):
+    trans_data = data
+
+    for index, row in data.iterrows():
+        point_vector = np.array([row[2], row[3], row[4]])
+        point_vector = np.append(point_vector, 1)
+        trans_point = np.matmul(transform_matrix, np.transpose(point_vector))
+        trans_data.iloc[index, 2], trans_data.iloc[index, 3], trans_data.iloc[index, 4] = trans_point[0], trans_point[1],trans_point[2]
+
+    return trans_data
+
+def plot_single_occupancy(data):
+    plt.close('all')
+
+    plt.figure(figsize=(6.8, 4.2))
+    plt.scatter(data.iloc[:, 2], data.iloc[:, 3], c=data.iloc[:, 4], cmap="magma", s=0.5)
+    plt.title("Lighthouse projection of occupancy in the x,y plane")
+    plt.xlabel("X axis")
+    plt.ylabel("Y axis")
+    plt.colorbar()
+
+    plt.figure(figsize=(6.8, 4.2))
+    plt.scatter(data.iloc[:, 3], data.iloc[:, 4], c=data.iloc[:, 2], cmap="magma", s=0.5)
+    plt.title("Lighthouse projection of occupancy in the y,z plane")
+    plt.xlabel("Y axis")
+    plt.ylabel("Z axis")
+    plt.gca().invert_yaxis()
+    plt.colorbar()
+
+    plt.figure(figsize=(6.8, 4.2))
+    plt.scatter(data.iloc[:, 2], data.iloc[:, 4], c=data.iloc[:, 3], cmap="magma", s=0.5)
+    plt.title("Lighthouse projection of occupancy in the x,z plane")
+    plt.xlabel("X axis")
+    plt.ylabel("Z axis")
+    plt.gca().invert_yaxis()
+    plt.colorbar()
 
 
-#light_vect_refy =
-#light_vect_ortho =
+tran_matrix = obtain_transform_matrix(calibration_directory, calibration_session, x_axis, y_axis, reference)
+
+t_data_1, t_data_2 = transform_data(data_1, tran_matrix), transform_data(data_2, tran_matrix)
+
+plot_single_occupancy(t_data_2)
+
+
