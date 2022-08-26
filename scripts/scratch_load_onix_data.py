3d2c5d779a6b04c095ff6c689e081e9aaf973188
Antonin Blot <antonin.blot@gmail.com>
2022-08-26 10:39:29 +0100

    Add an example scratch script to check how to load onix data

--- scripts/scratch_load_onix_data.py
+++ scripts/scratch_load_onix_data.py
@@ -9,81 +9,113 @@ Should load:
 - TS data
 """
 import matplotlib
-import pandas as pd
+
+from cottage_analysis.imaging.common import find_frames
+from cottage_analysis.imaging.common.find_frames import find_pulses
 
 matplotlib.use('MacOSX')
+import cv2
+import pandas as pd
 from cottage_analysis.io_module import harp
+from cottage_analysis.io_module import onix
+from cottage_analysis.io_module.video.io_func import deinterleave_camera
+from depth_analysis_2p.vis_stim import format_loggers
 import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 
 ROOT = Path('/Volumes/lab-znamenskiyp/')
+MOUSE = 'BRAC6692.4a'
+SESSION = 'S20220817'
+EPHYS_REC = 'R171408'
+VIS_STIM_REC = 'R165353_SpheresPermTubeReward'
+
 # ROOT = Path('/camp/lab/znamenskiyp/')
 DATA_PATH = ROOT / 'data/instruments/raw_data/projects'
-DATA_PATH = DATA_PATH / 'blota_onix_pilote/BRAC6692.4a/S20220817/'
+DATA_PATH = DATA_PATH / 'blota_onix_pilote' / MOUSE / SESSION
 PROCESS_PATH = ROOT / 'home/shared/projects'
-PROCESS_PATH = PROCESS_PATH / 'blota_onix_pilote/BRAC6692.4a/S20220817/'
+PROCESS_PATH = PROCESS_PATH / 'blota_onix_pilote' / MOUSE / SESSION
+
+# load ephys data
+ephys_data = onix.load_rhd2164(DATA_PATH / EPHYS_REC)
+# same for lighthouse photodiove
+ts_data = onix.load_ts4231(DATA_PATH / EPHYS_REC)
+
+# Load harp messages
+processed_messages = PROCESS_PATH / VIS_STIM_REC / 'processed_harp.npz'
+processed_messages.parent.mkdir(exist_ok=True)
+if Path(processed_messages).is_file():
+    harp_message = dict(np.load(processed_messages))
+else:
+    # slow part: read harp messages so save output and reload
+    harp_message = onix.load_harp(DATA_PATH / VIS_STIM_REC / ('%s_%s_%s_harpmessage.bin'
+                                  % (MOUSE, SESSION, VIS_STIM_REC)))
+    np.savez(processed_messages, **harp_message)
+
+# Load stimulation data
+
+param_file = DATA_PATH / VIS_STIM_REC / ('%s_%s_%s_NewParams.csv' % (MOUSE, SESSION,
+                                                                       VIS_STIM_REC))
+vis_stim = pd.read_csv(param_file)
+# add onix time to vis_stim
+# find triggers from harp
+# there was a double acquisition, get read of early trigger
+cutoff = 12547030
+harp_onix_clock = harp_message['onix_clock']
+harp_onix_clock = np.array(harp_onix_clock[harp_onix_clock > cutoff])
+real_time = np.arange(len(harp_onix_clock)) * 10e-3  # assume a perfect 100Hz
+# linear regression:
+t0 = harp_onix_clock[0]
+slope = np.nanmean(real_time / (harp_onix_clock - t0))
 
 
-# Harp
-behaviour_folder = DATA_PATH / 'R165353_SpheresPermTubeReward'
-bin_file = behaviour_folder / 'BRAC6692.4a_S20220817_R165353_SpheresPermTubeReward_harpmessage.bin'
-harp_message = harp.read_message(path_to_file=bin_file)
+def harp2onix(data):
+    """Convert harp timestamp in onix time"""
+    return (data - t0) * slope
 
-# harp contains a lot of stuff, split that
-# I'll make a dataframe to make it easier, it takes a while
-print('Making dataframe')
-harp_message = pd.DataFrame(harp_message)
-print('Done')
-# Each message has a message type that can be 'READ', 'WRITE', 'EVENT', 'READ_ERROR',
-# or 'WRITE_ERROR'.
-# We don't want error
-msg_types = harp_message.msg_type.unique()
-assert not np.any([m.endswith('ERROR') for m in msg_types])
-# READ events are the initial config loading at startup. We don't care
-harp_message = harp_message[harp_message.msg_type != 'READ']
 
-# WRITE messages are mostly the rewards.
-# The reward port is toggled by writing to register 36, let's focus on those events
-reward_message = harp_message[harp_message.address == 36]
-harp_reward_times = reward_message.timestamp_s.values
-
-# EVENT messages are analog and digital input.
-# Analog are the photodiode and the rotary encoder, both on address 44
-analog = harp_message[harp_message.address == 44]
-harp_analog_times = analog.timestamp_s.values
-analog = np.vstack(analog.data)
-harp_frame_photodiode = analog[:, 0]
-harp_rotary_encoder = analog[:, 1]
-
-# Digital input is on address 32, the data is 2 when the trigger is high
-di = harp_message[(harp_message.address == 32) & (harp_message.data == (2,))]
-harp_onix_trigger_times = di.timestamp_s.values
-
-# Ephys ephys.
-# This should be a simple binary file with uint16. Don't forget the U
-ephys = DATA_PATH / 'R171408' / r'rhd2164-ephys_2022-08-17T17_14_08.raw'
-n_pts = ephys.stat().st_size / 2  # divide by 2 for uint16
-ephys_data = np.memmap(ephys, dtype='uint16', mode='r', order='F',
-                       shape=(64, int(n_pts/64)))
-
-# plot the data
-time_window = 0.2
-fs = 30000
-v_shift = 400
+vis_stim['onix_time'] = harp2onix(vis_stim['HarpTime'])
+# plot what I did
 fig = plt.figure()
-ax = fig.add_subplot(2, 1, 1)
-data2plot = (np.array(ephys_data[:4, :int(fs*time_window)], float) - 2**15) * 0.195
-ax.plot(np.arange(int(fs*time_window))/fs,  data2plot.T + np.arange(4) * v_shift, lw=0.4)
-ax.set_title('Ephys')
+fig.subplots_adjust(wspace=0.3, hspace=0.4, top=0.98, right=0.98)
+ax = fig.add_subplot(2, 2, 1)
+ax.plot(harp_onix_clock, np.ones_like(harp_onix_clock), '|', label='Good triggers')
+ax.plot(harp_message['onix_clock'], np.zeros_like(harp_message['onix_clock']), '|',
+        label='All triggers')
+ax.axvline(cutoff, color='k')
+ax.set_xlabel('Harp time')
+ax.legend(loc=0)
+ax.yaxis.set_visible(False)
 
-ax = fig.add_subplot(2, 1, 2)
-ax.set_title('Harp')
+ax = fig.add_subplot(2, 2, 2)
 ax.clear()
-ht0 = harp_onix_trigger_times[0]  # 0 the hard at the first onix trigger ... but here
-# it does not quite work as the triggering was started, then stopped and restarted
+ax.hist(np.diff(harp_onix_clock)*1000)
+ax.set_xlabel('Intertrigger time (ms)')
 
-ax.plot(harp_analog_times - ht0, harp_frame_photodiode/800)
-ax.plot(harp_analog_times - ht0, harp_rotary_encoder/32000)
-ax.plot(harp_reward_times - ht0, np.zeros_like(harp_reward_times), 'o')
-ax.plot(harp_onix_trigger_times - ht0, np.zeros_like(harp_onix_trigger_times) - 1, '|')
+ax = fig.add_subplot(2, 2, 3)
+ax.clear()
+ax.plot(harp_onix_clock, real_time, '.')
+ax.set_xlabel('Harp time')
+ax.set_ylabel('Onix time')
+
+ax = fig.add_subplot(2, 2, 4)
+ax.clear()
+ax.plot(real_time, (harp2onix(harp_onix_clock) - real_time) * 1000, 'o')
+ax.set_xlabel('Onix time')
+ax.set_ylabel('Error (ms)')
+
+# Load spike sorted data
+kilosort_folder = PROCESS_PATH / EPHYS_REC / 'kilosort'
+ks_data = dict()
+for w in ['times', 'clusters']:
+    ks_data[w] = np.load(kilosort_folder / ('spike_%s.npy' % w)).reshape(-1)
+for w in ['group', 'info']:
+    ks_data[w] = pd.read_csv(kilosort_folder / ('cluster_%s.tsv' % w), sep='\t')
+
+# get an example unit, the good with the biggest amplitude
+good = ks_data['info'][ks_data['info'].group == 'good']
+cluster_id = good.cluster_id[good.amp.idxmax()]
+# get unit spike index in ephys
+spike_index = ks_data['times'][ks_data['clusters'] == cluster_id]
+clock = ephys_data['clock'].reshape(-1)
+spike_time = clock[spike_index]
