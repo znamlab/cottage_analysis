8c6bad274e8f84dc07f634b134f24d334db6fbe7
Antonin Blot <antonin.blot@gmail.com>
2022-09-23 14:44:11 +0100

    photodiode sampling can be specified in find_VS_frames
    Also add docstring

--- cottage_analysis/imaging/common/find_frames.py
+++ cottage_analysis/imaging/common/find_frames.py
@@ -20,8 +20,36 @@ from scipy.signal import find_peaks
 
 
 
-#%%
-def find_VS_frames(photodiode_df, frame_rate=144, upper_thr=200, lower_thr=50, plot=False, plot_start=0, plot_range=2000, plot_dir=None):
+# %%
+def find_VS_frames(photodiode_df, frame_rate=144, upper_thr=200, lower_thr=50,
+                   photodiode_sampling=1000, plot=False, plot_start=0, plot_range=2000,
+                   plot_dir=None):
+    """Find frame refresh based on photodiode signal
+
+    Signal is expected to alternate between high and low at each frame (flickering quad
+    between white and black)
+
+    Args:
+        photodiode_df (pd.DataFrame): dataframe with `HarpTime` and `Photodiode` fields
+        frame_rate (float): Expected frame rate. Peaks separated by less than half a
+                            frame will be ignored
+        upper_thr (float): Photodiode value above which the quad is considered white
+        lower_thr (float): Photodiode value below which the quad is considered black
+        photodiode_sampling (float): Sampling rate of the photodiode signal in Hz
+        plot (bool): Should a summary figure be generated?
+        plot_start (int): sample to start the plot
+        plot_range (int): samples to plot after plot_start
+        plot_dir (Path or str): directory to save the figure
+
+    Returns:
+        detected_frame (pd.DataFrame): a dataframe containing detected frame timing.
+            It contains:
+                - 'Photodiode': photodiode value at peak
+                - 'HarpTime': time of peak
+                - 'ElapsedTime': time of peak since first peak
+                - 'VisStim_Frame': peak index
+    """
+
     # Get elapsed time
     photodiode_df['ElapsedTime'] = None
     photodiode_df['ElapsedTime'] = photodiode_df.HarpTime - photodiode_df.HarpTime[0]
@@ -31,7 +59,7 @@ def find_VS_frames(photodiode_df, frame_rate=144, upper_thr=200, lower_thr=50, p
     photodiode = np.array(photodiode_df.Photodiode).reshape(-1)
 
     # Find peaks of photodiode
-    distance = int(1 / frame_rate * 2 / 0.001)
+    distance = int(1 / frame_rate * 2 * photodiode_sampling)
     high_peaks, _ = find_peaks(photodiode, height=upper_thr, distance=distance)
     first_frame = high_peaks[0]
     low_peaks, _ = find_peaks(-photodiode, height=-lower_thr, distance=distance)
@@ -41,33 +69,39 @@ def find_VS_frames(photodiode_df, frame_rate=144, upper_thr=200, lower_thr=50, p
     photodiode_df['FramePeak'] = None
     photodiode_df.FramePeak.iloc[high_peaks] = 1
     photodiode_df.FramePeak.iloc[low_peaks] = 0
-    photodiode_df_simple = photodiode_df[photodiode_df.FramePeak.notnull()]
-    photodiode_df_simple = photodiode_df_simple[photodiode_df_simple.FramePeak.diff() != 0]
-    photodiode_df_simple['VisStim_Frame'] = np.arange(len(photodiode_df_simple))
-    photodiode_df_simple = photodiode_df_simple.drop(columns=['FramePeak'])
-
+    detect_frames = photodiode_df[photodiode_df.FramePeak.notnull()]
+    detect_frames = detect_frames[detect_frames.FramePeak.diff() != 0]
+    detect_frames['VisStim_Frame'] = np.arange(len(detect_frames))
+    detect_frames = detect_frames.drop(columns=['FramePeak'])
 
     if plot:
         plt.figure()
-        plt.plot(elapsed_time[plot_start:(plot_start + plot_range)], photodiode[plot_start:(plot_start + plot_range)])
-        all_frame_idxs = photodiode_df_simple.index.values.reshape(-1)
+        plt.plot(elapsed_time[plot_start:(plot_start + plot_range)],
+                 photodiode[plot_start:(plot_start + plot_range)])
+        all_frame_idxs = detect_frames.index.values.reshape(-1)
         take_start = np.argmin(np.abs(all_frame_idxs - plot_start))
         take_stop = np.argmin(np.abs(all_frame_idxs - plot_start - plot_range))
         take_idxs = all_frame_idxs[take_start:take_stop]
 
-        plot_peaks = np.intersect1d(all_frame_idxs, (np.arange(plot_start, (plot_start + plot_range), step=1)))
+        plot_peaks = np.intersect1d(all_frame_idxs, (
+            np.arange(plot_start, (plot_start + plot_range), step=1)))
         # plt.figure()
-        plt.plot(photodiode_df_simple.loc[take_idxs, 'ElapsedTime'], photodiode_df_simple.loc[take_idxs, 'Photodiode'])
+        plt.plot(detect_frames.loc[take_idxs, 'ElapsedTime'],
+                 detect_frames.loc[take_idxs, 'Photodiode'])
         plt.plot(elapsed_time[plot_peaks], photodiode[plot_peaks], "x")
         plt.plot(elapsed_time[plot_start:(plot_start + plot_range)],
-                 np.zeros_like(photodiode[plot_start:(plot_start + plot_range)]) + upper_thr, "--", color="gray")
+                 np.zeros_like(
+                     photodiode[plot_start:(plot_start + plot_range)]) + upper_thr, "--",
+                 color="gray")
         plt.plot(elapsed_time[plot_start:(plot_start + plot_range)],
-                 np.zeros_like(photodiode[plot_start:(plot_start + plot_range)]) + lower_thr, "--", color="gray")
+                 np.zeros_like(
+                     photodiode[plot_start:(plot_start + plot_range)]) + lower_thr, "--",
+                 color="gray")
         plt.xlabel('Time(s)')
     if plot_dir is not None:
         plt.savefig(Path(plot_dir) / 'Frame_finder_check.png')
 
-    return photodiode_df_simple
+    return detect_frames
 
 
 
