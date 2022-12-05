59075675f3f56e373c7208dbc43dedab49d8f715
Antonin Blot <antonin.blot@crick.ac.uk>
2022-12-05 13:46:11 +0000

    STS does not return partial data
    If the event is too close to the borders of the recording

--- cottage_analysis/utilities/continuous_data_analysis.py
+++ cottage_analysis/utilities/continuous_data_analysis.py
@@ -5,7 +5,7 @@ from numpy.lib.stride_tricks import sliding_window_view
 from scipy.signal import butter, sosfiltfilt, bessel
 
 
-def crosscorrelation(signal1, signal2, maxlag, expected_lag=0, normalisation='pearson'):
+def crosscorrelation(signal1, signal2, maxlag, expected_lag=0, normalisation="pearson"):
     """Crosscorrelation limited to +/- maxlag around expected lag
 
     Performed the crosscorrelation of signal1 and signal2 (2 arrays of same length) but
@@ -36,27 +36,28 @@ def crosscorrelation(signal1, signal2, maxlag, expected_lag=0, normalisation='pe
     assert len(signal2) == len(signal1)
     assert len(signal2) > (maxlag * 2 + expected_lag)
     assert expected_lag >= 0
-    y = signal2[:len(signal2)-expected_lag]
-    x = signal1[maxlag+expected_lag:-maxlag + 1]
-    lags = scsi.correlation_lags(len(x), len(y), mode='valid')
+    y = signal2[: len(signal2) - expected_lag]
+    x = signal1[maxlag + expected_lag : -maxlag + 1]
+    lags = scsi.correlation_lags(len(x), len(y), mode="valid")
     lags += maxlag + expected_lag
 
-    circ_y = sliding_window_view(y, maxlag*2)
+    circ_y = sliding_window_view(y, maxlag * 2)
     # With that circ_y[:, n] = signal2[n:n - maxlag * 2 + 1], so circ_y shifts the signal
     # forward, we want to shift it backward, reverse that.
     circ_y = circ_y[:, ::-1].T
-    if normalisation.lower() == 'dot':
+    if normalisation.lower() == "dot":
         corr = circ_y.dot(x)
-    elif normalisation.lower() == 'pearson':
+    elif normalisation.lower() == "pearson":
         # calculate pearson correlation coefficient
-        corr = (circ_y.dot(x) / x.size - (circ_y.mean(axis=1) * x.mean())) / \
-               (np.std(circ_y, axis=1) * np.std(x))
+        corr = (circ_y.dot(x) / x.size - (circ_y.mean(axis=1) * x.mean())) / (
+            np.std(circ_y, axis=1) * np.std(x)
+        )
     else:
-        raise IOError('Normalisation must be `dot` or `pearson`')
+        raise IOError("Normalisation must be `dot` or `pearson`")
     return corr, lags
 
 
-def filter(data, sampling, lowcut=None, highcut=None, design='butter', axis=-1):
+def filter(data, sampling, lowcut=None, highcut=None, design="butter", axis=-1):
     """Wrapper around butter and filtfilt to filter continuous data
 
     Args:
@@ -71,31 +72,39 @@ def filter(data, sampling, lowcut=None, highcut=None, design='butter', axis=-1):
         filtered_data (np.array
     """
     n = int(lowcut is not None) * 2 + int(highcut is not None)
-    filter_type = [None, 'lowpass', 'highpass', 'bandpass'][n]
+    filter_type = [None, "lowpass", "highpass", "bandpass"][n]
     if filter_type is None:
-        warnings.warn('Both frequencies are None. Do nothing')
+        warnings.warn("Both frequencies are None. Do nothing")
         return data
-    if filter_type == 'lowpass':
-        freq = highcut/sampling
-    elif filter_type == 'highpass':
-        freq = lowcut/sampling
+    if filter_type == "lowpass":
+        freq = highcut / sampling
+    elif filter_type == "highpass":
+        freq = lowcut / sampling
     else:
-        freq = (lowcut/sampling, highcut/sampling)
-    if design.lower() == 'butter':
+        freq = (lowcut / sampling, highcut / sampling)
+    if design.lower() == "butter":
         filt_func = butter
-    elif design.lower() == 'bessel':
+    elif design.lower() == "bessel":
         filt_func = bessel
     else:
-        raise IOError('`design` must be `bessel` or `butter`')
-    sos = filt_func(N=4, Wn=freq, btype=filter_type, output='sos')
+        raise IOError("`design` must be `bessel` or `butter`")
+    sos = filt_func(N=4, Wn=freq, btype=filter_type, output="sos")
     fdata = sosfiltfilt(sos, data, axis=axis)
     return fdata
 
 
-def do_sta(analog_signal, event, window=[-.5, .5], verbose=True,
-           s_rate=None, event_in_samples=False, t0=0, dtype=float,
-           return_variance=False):
-    """"Do a stimulus triggered average of analogSignal over the given time
+def do_sta(
+    analog_signal,
+    event,
+    window=[-0.5, 0.5],
+    verbose=True,
+    s_rate=None,
+    event_in_samples=False,
+    t0=0,
+    dtype=float,
+    return_variance=False,
+):
+    """ "Do a stimulus triggered average of analogSignal over the given time
     window
 
     Can also estimate the variance using Welford algorithm.
@@ -103,10 +112,10 @@ def do_sta(analog_signal, event, window=[-.5, .5], verbose=True,
     Using extract_sts + np.std might be better for things fitting in memory
     """
 
-    times = np.array(event, dtype='float')
+    times = np.array(event, dtype="float")
 
     if s_rate is not None:
-        winind = np.array(np.asarray(window) * s_rate, dtype='int')
+        winind = np.array(np.asarray(window) * s_rate, dtype="int")
     else:
         assert all(np.array(window, dtype=int) == np.array(window))
         winind = np.array(window, dtype=int)
@@ -119,21 +128,23 @@ def do_sta(analog_signal, event, window=[-.5, .5], verbose=True,
         if not event_in_samples:
             if s_rate is None:
                 raise IOError(
-                    'Event is not in samples and I do not know the sampling rate')
+                    "Event is not in samples and I do not know the sampling rate"
+                )
             ind_ev = int((e - t0) * s_rate)
         else:
             ind_ev = int(e)
         if ind_ev + winind[1] > len(analog_signal):
             if verbose:
-                print('skip last cause it does not fit')
+                print("skip last cause it does not fit")
             continue
         if ind_ev + winind[0] < 0:
             if verbose:
-                print('skip first cause it does not fit')
+                print("skip first cause it does not fit")
             continue
 
-        x = np.array(analog_signal[ind_ev + winind[0]: ind_ev + winind[1]],
-                     dtype=dtype)  # current value
+        x = np.array(
+            analog_signal[ind_ev + winind[0] : ind_ev + winind[1]], dtype=dtype
+        )  # current value
         if mean is None:  # initialise zero values
             mean = np.zeros_like(x)
             if return_variance:
@@ -155,19 +166,27 @@ def do_sta(analog_signal, event, window=[-.5, .5], verbose=True,
     return mean
 
 
-def extract_sts(analog_signal, event, window=[-.5, .5], verbose=True,
-                s_rate=None, event_in_samples=False, event_t0=0, dtype=float):
-    """"Find stimulus triggered snippets of analogSignal over the given time
+def extract_sts(
+    analog_signal,
+    event,
+    window=[-0.5, 0.5],
+    verbose=True,
+    s_rate=None,
+    event_in_samples=False,
+    event_t0=0,
+    dtype=float,
+):
+    """ "Find stimulus triggered snippets of analogSignal over the given time
     window
 
     Returns the individual elements that can be averaged to generate the STA
 
     event_t0 is subtracted to every time. It needs to be like event, in sample or time
     """
-    times = np.array(event, dtype='float')
+    times = np.array(event, dtype="float")
 
     if s_rate is not None:
-        winind = np.array(np.asarray(window) * s_rate, dtype='int')
+        winind = np.array(np.asarray(window) * s_rate, dtype="int")
     else:
         assert all(np.array(window, dtype=int) == np.array(window))
         winind = np.array(window, dtype=int)
@@ -179,23 +198,27 @@ def extract_sts(analog_signal, event, window=[-.5, .5], verbose=True,
     elif ndim == 1:
         out = np.zeros((n, np.diff(winind)[0]), dtype=dtype)
     else:
-        raise IOError('Analog signal must have 1 or 2 dim')
+        raise IOError("Analog signal must have 1 or 2 dim")
+    ok_ind = []
     for ind, e in enumerate(times):
         if not event_in_samples:
             if s_rate is None:
                 raise IOError(
-                    'Event is not in samples and I do not know the sampling rate')
+                    "Event is not in samples and I do not know the sampling rate"
+                )
             ind_ev = int((e - event_t0) * s_rate)
         else:
             ind_ev = int(e - event_t0)
         if ind_ev + winind[1] > len(analog_signal):
             if verbose:
-                print('skip last cause it does not fit')
+                print("skip last cause it does not fit")
             continue
         elif ind_ev + winind[0] < 0:
             if verbose:
-                print('skip first cause it does not fit')
+                print("skip first cause it does not fit")
             continue
-        out[ind] = np.asarray(analog_signal[ind_ev + winind[0]: ind_ev + winind[1]],
-                              dtype=dtype)
-    return out
+        ok_ind.append(ind)
+        out[ind] = np.asarray(
+            analog_signal[ind_ev + winind[0] : ind_ev + winind[1]], dtype=dtype
+        )
+    return out[ok_ind]
