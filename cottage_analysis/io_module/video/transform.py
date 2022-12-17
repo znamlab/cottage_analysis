f42999ce86790c0bea0c813d4348077ada92cf2a
Antonin Blot <antonin.blot@crick.ac.uk>
2022-12-17 21:32:23 +0000

    Formatting transform.py

--- cottage_analysis/io_module/video/transform.py
+++ cottage_analysis/io_module/video/transform.py
@@ -12,9 +12,17 @@ import os
 import cottage_analysis.imaging.common.chunk_data as chunk_data
 
 
-
-def transpose(data_folder, data_filename, data_shape, save_folder, save_filename, chunk_size, dtype=np.uint16, verbose=1):
-    '''
+def transpose(
+    data_folder,
+    data_filename,
+    data_shape,
+    save_folder,
+    save_filename,
+    chunk_size,
+    dtype=np.uint16,
+    verbose=1,
+):
+    """
     Transpose a stored array (column major) into frame-major form.
 
     Parameters
@@ -40,50 +48,43 @@ def transpose(data_folder, data_filename, data_shape, save_folder, save_filename
     -------
     None.
 
-    '''
-    
+    """
+
     # load data into memmap
-    data_path = data_folder+data_filename
-    assert(os.path.isfile(data_path))
-    data = np.memmap(data_path,dtype=dtype, mode='r')
-    
+    data_path = data_folder + data_filename
+    assert os.path.isfile(data_path)
+    data = np.memmap(data_path, dtype=dtype, mode="r")
+
     # reshape data into (row, col, frame)
     height = data_shape[0]
     width = data_shape[1]
     frames = data_shape[2]
-    data = data.reshape((height, width,-1),order='F')
-    
+    data = data.reshape((height, width, -1), order="F")
+
     # create an empty memmap
-    save_path = save_folder+save_filename
+    save_path = save_folder + save_filename
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
-    new_fp = np.memmap(save_path, dtype=dtype, mode='w+', shape=data.shape, order='C')
-    
+    new_fp = np.memmap(save_path, dtype=dtype, mode="w+", shape=data.shape, order="C")
+
     # save frame-major ordered data to new memmap
     if chunk_size is not None:
         chunk_list = chunk_data.chunk_data(arr=np.arange(frames), chunk_size=chunk_size)
-        
-        for ichunk in range(0,len(chunk_list)):
+
+        for ichunk in range(0, len(chunk_list)):
             start_frame = chunk_list[ichunk][0]
-            stop_frame = chunk_list[ichunk][-1]+1
-            new_fp[:,:,start_frame:stop_frame] = data[:,:,start_frame:stop_frame]
+            stop_frame = chunk_list[ichunk][-1] + 1
+            new_fp[:, :, start_frame:stop_frame] = data[:, :, start_frame:stop_frame]
             new_fp.flush()
-            
-            if verbose==1:
-                print('finished: chunk '+str(ichunk)+'/'+str(len(chunk_list)-1),flush=True)
-        
+
+            if verbose == 1:
+                print(
+                    "finished: chunk " + str(ichunk) + "/" + str(len(chunk_list) - 1),
+                    flush=True,
+                )
+
     else:
-        new_fp[:,:,:] = data[:,:,:]
+        new_fp[:, :, :] = data[:, :, :]
         new_fp.flush()
-            
-    print('---Transpose finished.---',flush=True)
 
-    
-    
-    
-    
-    
-    
-    
-    
-
+    print("---Transpose finished.---", flush=True)
