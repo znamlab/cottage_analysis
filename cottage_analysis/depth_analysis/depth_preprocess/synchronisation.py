3c9501967c2812fed2ca1d97551d45d497c777b0
yiran25 <yiran.he@crick.ac.uk>
2023-03-28 23:27:49 +0100

    Add code to generate trials_df

--- cottage_analysis/depth_analysis/depth_preprocess/synchronisation.py
+++ cottage_analysis/depth_analysis/depth_preprocess/synchronisation.py
@@ -17,7 +17,7 @@ from cottage_analysis.imaging.common import imaging_loggers_formatting as format
 from cottage_analysis.depth_analysis.depth_preprocess import synchronisation
 
 
-def load_harpmessage(project, mouse, session, protocol, all_protocol_recording_entries=None, irecording=0, redo=False):
+def load_harpmessage(project, mouse, session, protocol, irecording=0, redo=False):
     '''Save harpmessage into a npz file, or load existing npz file. Then load harpmessage file as a np arrray.
 
     Args:
@@ -25,7 +25,6 @@ def load_harpmessage(project, mouse, session, protocol, all_protocol_recording_e
         mouse (str): mouse name
         session (str): session name (Sdate)
         protocol (str): protocol name
-        all_protocol_recording_entries (DataFrame, optional): All recording entries matching the protocol name in a given session. Defaults to None.
         irecording (int, optional): which recording is the current recording out of all entries in all_protocol_recording_entries. Defaults to 0.
         flexilims_session (flexilims_session, optional): flexilims session. Defaults to None.
         redo (bool, optional): re-transform harp bin file into npz file or not. Defaults to False.
@@ -34,6 +33,11 @@ def load_harpmessage(project, mouse, session, protocol, all_protocol_recording_e
         np.array: loaded harpmessages as numpy array
     '''
     flexilims_session = flz.get_flexilims_session(project_id=project)
+    all_protocol_recording_entries = generate_filepaths.get_all_recording_entries(project=project, 
+                                                                              mouse=mouse, 
+                                                                              session=session, 
+                                                                              protocol=protocol, 
+                                                                              flexilims_session=flexilims_session)
     # Find harpmessage path
     harpmessage_file = generate_filepaths.generate_logger_path(
         project=project,
@@ -82,7 +86,7 @@ def load_harpmessage(project, mouse, session, protocol, all_protocol_recording_e
     return harp_messages
 
 
-def find_monitor_frames(project, mouse, session, protocol, all_protocol_recording_entries=None, irecording=0, redo=True, redo_harpnpz=False):
+def find_monitor_frames(project, mouse, session, protocol, irecording=0, redo=True, redo_harpnpz=False):
     '''Synchronise monitor frame using the find_frames.sync_by_correlation, and save them into monitor_frames_df.pickle and monitor_db_dict.pickle under the path {trace_folder/'sync/monitor_frames/'}
 
     Args:
@@ -90,7 +94,6 @@ def find_monitor_frames(project, mouse, session, protocol, all_protocol_recordin
         mouse (str): mouse name
         session (str): session name (Sdate)
         protocol (str): protocol name
-        all_protocol_recording_entries (DataFrame, optional): All recording entries matching the protocol name in a given session. Defaults to None.
         irecording (int, optional): which recording is the current recording out of all entries in all_protocol_recording_entries. Defaults to 0.
         flexilims_session (flexilims_session, optional): flexilims session. Defaults to None.
         redo (bool, optional): re-sync the monitor frames or not. Defaults to True.
@@ -98,6 +101,11 @@ def find_monitor_frames(project, mouse, session, protocol, all_protocol_recordin
     '''
     # Find paths
     flexilims_session = flz.get_flexilims_session(project_id=project)
+    all_protocol_recording_entries = generate_filepaths.get_all_recording_entries(project=project, 
+                                                                              mouse=mouse, 
+                                                                              session=session, 
+                                                                              protocol=protocol, 
+                                                                              flexilims_session=flexilims_session)
     (
     rawdata_folder,
     protocol_folder,
@@ -161,7 +169,7 @@ def find_monitor_frames(project, mouse, session, protocol, all_protocol_recordin
         )
 
         # Save monitor frame dataframes
-        save_folder = protocol_folder/'sync/monitor_frames/'
+        save_folder = protocol_folder/'sync/monitor_frames'
         if not os.path.exists(save_folder):
             os.makedirs(save_folder)
         frames_df.to_pickle(save_folder/'monitor_frames_df.pickle')  
@@ -170,7 +178,7 @@ def find_monitor_frames(project, mouse, session, protocol, all_protocol_recordin
 
 
 
-def generate_vs_df(project, mouse, session, protocol, all_protocol_recording_entries=None, irecording=0):
+def generate_vs_df(project, mouse, session, protocol, irecording=0):
     '''Generate a dataframe that contains information for each monitor frame. This requires monitor frames to be synced first.
 
     Args:
@@ -178,21 +186,20 @@ def generate_vs_df(project, mouse, session, protocol, all_protocol_recording_ent
         mouse (str): mouse name
         session (str): session name (Sdate)
         protocol (str): protocol name
-        all_protocol_recording_entries (DataFrame, optional): All recording entries matching the protocol name in a given session. Defaults to None.
         irecording (int, optional): which recording is the current recording out of all entries in all_protocol_recording_entries. Defaults to 0.
 
     Returns:
         DataFrame: contains information for each monitor frame.
     '''
     flexilims_session = flz.get_flexilims_session(project_id=project)
+    all_protocol_recording_entries = generate_filepaths.get_all_recording_entries(project=project, 
+                                                                            mouse=mouse, 
+                                                                            session=session, 
+                                                                            protocol=protocol, 
+                                                                            flexilims_session=flexilims_session)
     sess_children = generate_filepaths.get_session_children(project=project, mouse=mouse, session=session, flexilims_session=flexilims_session)
     sess_children_protocols = sess_children[sess_children['name'].str.contains('(SpheresPermTubeReward|Fourier|Retinotopy)')]
     folder_no = sess_children_protocols.index.get_loc(sess_children_protocols[sess_children_protocols.id == all_protocol_recording_entries.iloc[irecording].id].index[0])
-    all_protocol_recording_entries = generate_filepaths.get_all_recording_entries(project=project, 
-                                                                                mouse=mouse, 
-                                                                                session=session, 
-                                                                                protocol=protocol, 
-                                                                                flexilims_session=flexilims_session)
     (
     rawdata_folder,
     protocol_folder,
@@ -234,7 +241,8 @@ def generate_vs_df(project, mouse, session, protocol, all_protocol_recording_ent
     param_log_simple = param_log_simple.rename(columns={'HarpTime':'harptime_sphere','Radius':'depth'})
     param_log_simple['onset_time'] = param_log_simple['harptime_sphere']
     vs_df = pd.merge_asof(left=vs_df, right=param_log_simple, on='onset_time', direction='backward',allow_exact_matches=False) # Does not allow exact match of sphere rendering time and frame onset time?
-
+    vs_df['depth'] = vs_df['depth']/100  #convert cm to m
+    
     # Align imaging frame time with monitor frame onset time (imaging frame time later than monitor frame onset time)
     ops = np.load(suite2p_folder/"ops.npy", allow_pickle=True)
     ops = ops.item()
@@ -274,4 +282,117 @@ def generate_vs_df(project, mouse, session, protocol, all_protocol_recording_ent
     
     # Rename
     vs_df = vs_df.rename(columns={'closest_frame':'monitor_frame'})
+    
+    # Save df to pickle
+    save_folder = protocol_folder/'sync'
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    vs_df.to_pickle(save_folder/'vs_df.pickle') 
     return vs_df
+
+
+def generate_trials_df(project, mouse, session, protocol, vs_df, irecording=0):
+    '''Generate a dataframe that contains information for each trial. This requires monitor frames to be synced and vs_df to be generated first.
+
+    Args:
+        project (str): project name
+        mouse (str): mouse name
+        session (str): session name (Sdate)
+        protocol (str): protocol name
+        irecording (int, optional): which recording is the current recording out of all entries in all_protocol_recording_entries. Defaults to 0.
+        vs_df (DataFrame): contains information for each monitor frame.
+
+    Returns:
+        DataFrame: contains information for each trial.
+    '''
+    flexilims_session = flz.get_flexilims_session(project_id=project)
+    all_protocol_recording_entries = generate_filepaths.get_all_recording_entries(project=project, 
+                                                                            mouse=mouse, 
+                                                                            session=session, 
+                                                                            protocol=protocol, 
+                                                                            flexilims_session=flexilims_session)
+    sess_children = generate_filepaths.get_session_children(project=project, mouse=mouse, session=session, flexilims_session=flexilims_session)
+    sess_children_protocols = sess_children[sess_children['name'].str.contains('(SpheresPermTubeReward|Fourier|Retinotopy)')]
+    folder_no = sess_children_protocols.index.get_loc(sess_children_protocols[sess_children_protocols.id == all_protocol_recording_entries.iloc[irecording].id].index[0])
+    (
+    rawdata_folder,
+    protocol_folder,
+    analysis_folder,
+    suite2p_folder,
+    trace_folder,
+    ) = generate_filepaths.generate_file_folders(
+        project=project,
+        mouse=mouse,
+        session=session,
+        protocol=protocol,
+        all_protocol_recording_entries=all_protocol_recording_entries, 
+        recording_no=0, 
+    )
+    
+
+    # trials_df
+    trials_df = pd.DataFrame(columns=['trial_no','depth', 
+                                    'start_time_stim', 'stop_time_stim', 'start_time_blank', 'stop_time_blank',
+                                    'start_imaging_frame_stim', 'stop_imaging_frame_stim', 'start_imaging_frame_blank', 'stop_imaging_frame_blank',
+                                    'RS_array_stim', 'RS_array_blank', 'RS_eye_array_stim',
+                                    'OF_array_stim', 
+                                    'spheres_no', 'closed_loop'])
+
+    # Find the start and stop of each trial
+    blank_time = 10 #s
+        
+    vs_df['stim'] = np.nan
+    vs_df.loc[vs_df.depth.notnull(), 'stim'] = 1
+    vs_df.loc[vs_df.depth < 0, 'stim'] = 0
+    vs_df_simple = vs_df[(vs_df['stim'].diff() != 0) & (vs_df['stim'].notnull())]
+    vs_df_simple.depth = np.round(vs_df_simple.depth, 2)
+
+    start_idx_stim = vs_df_simple[(vs_df_simple['stim'] == 1)].index
+    start_idx_blank= vs_df_simple[(vs_df_simple['stim'] == 0)].index
+    if len(start_idx_stim) != len(start_idx_blank):
+        if (len(start_idx_stim) - len(start_idx_blank))==1:
+            stop_idx_blank = start_idx_stim[1:]-1
+            start_idx_stim = start_idx_stim[:len(start_idx_blank)]
+        else:
+            print('Warning: incorrect stimulus trial structure! Double check!')
+    else:
+        stop_idx_blank = start_idx_stim[1:]-1
+        last_blank_stop_time = vs_df.loc[start_idx_blank[-1]].onset_time+blank_time
+        stop_idx_blank = np.append(stop_idx_blank, (np.abs(vs_df['onset_time'] -last_blank_stop_time)).idxmin())
+    stop_idx_stim = start_idx_blank-1
+
+
+    # Assign trial no, depth, start/stop time, start/stop imaging frame to trials_df
+    # !!! trials_df RS/OF should be changed!!! now it's only choosing one value from each imaging frame --> should we average?
+    trials_df.trial_no = np.arange(len(start_idx_stim))
+    trials_df.depth = vs_df.loc[start_idx_stim].depth.values
+    trials_df.start_time_stim = vs_df.loc[start_idx_stim].onset_time.values
+    trials_df.stop_time_stim = vs_df.loc[stop_idx_stim].onset_time.values
+    trials_df.start_time_blank = vs_df.loc[start_idx_blank].onset_time.values
+    trials_df.stop_time_blank = vs_df.loc[stop_idx_blank].onset_time.values
+    trials_df.start_imaging_frame_stim = vs_df.loc[start_idx_stim].imaging_frame.values
+    trials_df.stop_imaging_frame_stim = vs_df.loc[stop_idx_stim].imaging_frame.values
+    trials_df.start_imaging_frame_blank = vs_df.loc[start_idx_blank].imaging_frame.values
+    trials_df.stop_imaging_frame_blank = vs_df.loc[stop_idx_blank].imaging_frame.values
+
+    vs_img_frames = vs_df[vs_df.imaging_frame.diff()!=0].set_index('imaging_frame')
+    vs_img_frames['RS'] = vs_img_frames.mouse_z.diff()/vs_img_frames.onset_time.diff()
+    vs_img_frames['RS_eye'] = vs_img_frames.eye_z.diff()/vs_img_frames.onset_time.diff()
+    trials_df.RS_array_stim = trials_df.apply(lambda x: vs_img_frames.RS.loc[x.start_imaging_frame_stim:x.stop_imaging_frame_stim].values, axis=1) # This is not RS, this is mouseZ!!!
+    trials_df.RS_array_blank = trials_df.apply(lambda x: vs_img_frames.RS.loc[x.start_imaging_frame_blank:x.stop_imaging_frame_blank].values, axis=1)
+    trials_df.RS_eye_array_stim = trials_df.apply(lambda x: vs_img_frames.RS_eye.loc[x.start_imaging_frame_stim:x.stop_imaging_frame_stim].values, axis=1)
+    trials_df.OF_array_stim = trials_df.apply(lambda x: x['RS_eye_array_stim']/x['depth'], axis=1)
+
+    if 'Playback' in protocol:
+        trials_df.closed_loop = 0
+    else:
+        trials_df.closed_loop = 1
+        
+    
+    # Save df to pickle
+    save_folder = protocol_folder/'sync'
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    trials_df.to_pickle(save_folder/'trials_df.pickle') 
+
+    return trials_df
