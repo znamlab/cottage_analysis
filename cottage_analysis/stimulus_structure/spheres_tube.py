9f5fe4ff66d5339de389077baecdae5bd1050c04
yiran25 <yiran.he@crick.ac.uk>
2023-05-10 00:34:27 +0100

    Minor change to regenerate_frames to avoid this_frame being None when there's frame drop

--- cottage_analysis/stimulus_structure/spheres_tube.py
+++ cottage_analysis/stimulus_structure/spheres_tube.py
@@ -110,7 +110,7 @@ def regenerate_frames(
     draw_sph_jit = jax.jit(draw_spheres)
     for frame_index in tqdm(frame_indices):
         corridor = trials_df.loc[int(trial_index[frame_index])]
-        logger = param_logger.iloc[corridor.param_log_start : log_ends[frame_index]]
+        logger = param_logger.iloc[corridor.param_log_start : np.max([log_ends[frame_index],corridor.param_log_start+1])]
         sphere_coordinates = np.array(logger[["X", "Y", "Z"]].values, dtype=float)
         sphere_coordinates[:, 2] = (
             sphere_coordinates[:, 2] - mouse_position[frame_index]
