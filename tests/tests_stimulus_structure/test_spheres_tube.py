4af80413d01a9895d9193b8ba6eec0fcb9f14e63
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2023-07-17 13:13:29 +0100

    [refactor] rename references to `sphere_tube`

--- tests/tests_stimulus_structure/test_spheres_tube.py
+++ tests/tests_stimulus_structure/test_spheres_tube.py
@@ -12,27 +12,27 @@ from cottage_analysis.io_module import harp
 import pandas as pd
 import flexiznam as flm
 from scipy.sparse import bsr_array
-from cottage_analysis.stimulus_structure import spheres_tube as stu
+from cottage_analysis.analysis import spheres as stu
 from cottage_analysis.utilities.time_series_analysis import searchclosest
 from cottage_analysis.utilities import continuous_data_analysis as cda
 
-PROJECT = 'blota_onix_pilote'
-MOUSE = 'BRAC6692.4a'
-SESSION = 'S20220831'
-RECORDING = 'R163332_SpheresPermTubeReward'
-MESSAGES = '%s_%s_%s_harpmessage.bin' % (MOUSE, SESSION, RECORDING)
-FRAME_LOG = '%s_%s_%s_FrameLog.csv' % (MOUSE, SESSION, RECORDING)
-PARAMS_LOG = '%s_%s_%s_NewParams.csv' % (MOUSE, SESSION, RECORDING)
-ROTARY_LOG = '%s_%s_%s_RotaryEncoder.csv' % (MOUSE, SESSION, RECORDING)
+PROJECT = "blota_onix_pilote"
+MOUSE = "BRAC6692.4a"
+SESSION = "S20220831"
+RECORDING = "R163332_SpheresPermTubeReward"
+MESSAGES = "%s_%s_%s_harpmessage.bin" % (MOUSE, SESSION, RECORDING)
+FRAME_LOG = "%s_%s_%s_FrameLog.csv" % (MOUSE, SESSION, RECORDING)
+PARAMS_LOG = "%s_%s_%s_NewParams.csv" % (MOUSE, SESSION, RECORDING)
+ROTARY_LOG = "%s_%s_%s_RotaryEncoder.csv" % (MOUSE, SESSION, RECORDING)
 
 
 def test_recreate_stimulus():
     # load data
-    data_root = flm.PARAMETERS['data_root']
-    raw_data = Path(data_root['raw']) / PROJECT / MOUSE / SESSION / RECORDING
+    data_root = flm.PARAMETERS["data_root"]
+    raw_data = Path(data_root["raw"]) / PROJECT / MOUSE / SESSION / RECORDING
     msg = raw_data / MESSAGES
-    p_msg = Path(data_root['processed']) / PROJECT / MOUSE / SESSION / RECORDING
-    p_msg = p_msg / (msg.stem + '.npz')
+    p_msg = Path(data_root["processed"]) / PROJECT / MOUSE / SESSION / RECORDING
+    p_msg = p_msg / (msg.stem + ".npz")
     if p_msg.is_file():
         harp_messages = np.load(p_msg)
     else:
@@ -43,38 +43,45 @@ def test_recreate_stimulus():
     azimuth_limits = [-120, 120]
     elevation_limits = [-40, 40]
     resolution = 1
-    time_column = 'HarpTime'
+    time_column = "HarpTime"
 
     frame_log = pd.read_csv(raw_data / FRAME_LOG)
     params = pd.read_csv(raw_data / PARAMS_LOG)
     rot_log = pd.read_csv(raw_data / ROTARY_LOG)
     corridor_df = stu.trial_structure(params, time_column=time_column)
-    all_frames = frame_log['HarpTime']
+    all_frames = frame_log["HarpTime"]
 
-    mouse_pos_cm = harp_messages['rotary_meter'].cumsum() * 100
+    mouse_pos_cm = harp_messages["rotary_meter"].cumsum() * 100
     frame_times = all_frames[::10][20000:22000]
-    out_shape = (len(frame_times), int(np.diff(elevation_limits) / resolution),
-                 int(np.diff(azimuth_limits) / resolution))
-    outsize = np.prod(out_shape)*2/1024**3
+    out_shape = (
+        len(frame_times),
+        int(np.diff(elevation_limits) / resolution),
+        int(np.diff(azimuth_limits) / resolution),
+    )
+    outsize = np.prod(out_shape) * 2 / 1024**3
     if outsize > 10:
-        print('big image: %.2f Gb' % outsize)
-    output = np.zeros(out_shape, dtype='int16')
+        print("big image: %.2f Gb" % outsize)
+    output = np.zeros(out_shape, dtype="int16")
     start_time = time.time()
-    frames = stu.regenerate_frames(frame_times,
-                                   params,
-                                   mouse_pos_cm,
-                                   mouse_pos_time=harp_messages['analog_time'],
-                                   corridor_df=corridor_df, time_column='HarpTime',
-                                   resolution=resolution, sphere_size=sphere_size,
-                                   azimuth_limits=(-120, 120),
-                                   elevation_limits=(-40, 40),
-                                   output=output)
+    frames = stu.regenerate_frames(
+        frame_times,
+        params,
+        mouse_pos_cm,
+        mouse_pos_time=harp_messages["analog_time"],
+        corridor_df=corridor_df,
+        time_column="HarpTime",
+        resolution=resolution,
+        sphere_size=sphere_size,
+        azimuth_limits=(-120, 120),
+        elevation_limits=(-40, 40),
+        output=output,
+    )
     end_time = time.time()
-    print('That took %s s' % (end_time-start_time))
+    print("That took %s s" % (end_time - start_time))
     fig = plt.figure()
-    plt.imshow(frames.mean(axis=0), extent=azimuth_limits+elevation_limits)
-    fig.savefig(str(p_msg.with_name('temp.png')))
+    plt.imshow(frames.mean(axis=0), extent=azimuth_limits + elevation_limits)
+    fig.savefig(str(p_msg.with_name("temp.png")))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_recreate_stimulus()
