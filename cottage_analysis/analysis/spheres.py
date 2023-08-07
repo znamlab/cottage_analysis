44dab4d24cb0fdfec97664c100d9ae2385f8499b
yiran25 <yiran.he@crick.ac.uk>
2023-08-07 18:55:09 +0100

    [refactor] refactor concatenate_sync function to sync_all_recordings based on the new generate_trials_df functions

--- cottage_analysis/analysis/spheres.py
+++ cottage_analysis/analysis/spheres.py
@@ -547,58 +547,91 @@ def search_param_log_trials(recording, trials_df, flexilims_session):
     return trials_df
 
 
-def concatenate_sync(
+def sync_all_recordings(
     session_name,
-    flz_session=None,
+    flexilims_session=None,
     project=None,
+    filter_datasets=None,
     recording_type="two_photon",
     protocol_base="SpheresPermTubeReward",
+    photodiode_protocol=5,
+    return_volumes=True,
 ):
+    """Concatenate synchronisation results for all recordings in a session.
+
+    Args:
+        session_name (str): {mouse}_{session}
+        flexilims_session (flexilims_session, optional): flexilims session. Defaults to None.
+        project (str): project name. Defaults to None. Must be provided if flexilims_session is None.
+        filter_datasets (dict): dictionary of filter keys and values to filter for the desired suite2p dataset (e.g. {'anatomical':3}) Default to None.
+        recording_type (str, optional): Type of the recording. Defaults to "two_photon".
+        protocol_base (str, optional): Base of the protocol. Defaults to "SpheresPermTubeReward".
+        photodiode_protocol (int): number of photodiode quad colors used for monitoring frame refresh.
+            Either 2 or 5 for now. Defaults to 5.
+        return_volumes (bool): if True, return only the first frame of each imaging volume. Defaults to True.
+
+    Returns:
+        (pd.DataFrame, pd.DataFrame): tuple of two dataframes, one concatenated vs_df for all recordings, one concatenated trials_df for all recordings.
+    """
     assert flexilims_session is not None or project is not None
     if flexilims_session is None:
         flexilims_session = flz.get_flexilims_session(project_id=project)
 
     exp_session = flz.get_entity(
-        datatype="session", name=session_name, flexilims_session=flz_session
+        datatype="session", name=session_name, flexilims_session=flexilims_session
     )
     recordings = flz.get_entities(
         datatype="recording",
         origin_id=exp_session["id"],
         query_key="recording_type",
         query_value=recording_type,
-        flexilims_session=flz_session,
+        flexilims_session=flexilims_session,
     )
+    recordings = recordings[recordings.name.str.contains(protocol_base)]
 
     for i, recording_name in enumerate(recordings.name):
-        if protocol_base in recording_name:
-            recording = flz.get_entity(
-                datatype="recording", name=recording_name, flexilims_session=flz_session
-            )
+        recording = flz.get_entity(
+            datatype="recording",
+            name=recording_name,
+            flexilims_session=flexilims_session,
+        )
 
-            print(f"Processing recording {i+1}/{len(recordings)}")
-            vs_df = synchronisation.generate_vs_df(
-                recording=recording,
-                photodiode_protocol=photodiode_protocol,
-                flexilims_session=None,
-                project=project,
-                sync_imaging=True,
-                filter_datasets=filter_datasets,
-            )
-            trials_df = spheres.generate_trials_df(
-                recording=recording,
-                vs_df=vs_df,
-                flexilims_session=None,
-                project=project,
-                filter_datasets=filter_datasets,
-            )
+        print(f"Processing recording {i+1}/{len(recordings)}")
+        vs_df = synchronisation.generate_vs_df(
+            recording=recording,
+            photodiode_protocol=photodiode_protocol,
+            flexilims_session=flexilims_session,
+            project=project,
+        )
 
-            if i == 0:
-                vs_df_all = vs_df
-                trials_df_all = trials_df
-            else:
-                vs_df_all = pd.concat([vs_df_all, vs_df], ignore_index=True)
-                trials_df_all = pd.concat([trials_df_all, trials_df], ignore_index=True)
-            continue
+        imaging_df = synchronisation.generate_imaging_df(
+            vs_df=vs_df,
+            recording=recording,
+            flexilims_session=flexilims_session,
+            filter_datasets=filter_datasets,
+            return_volumes=return_volumes,
+        )
+
+        imaging_df = format_imaging_df(
+            recording=recording, imaging_df=imaging_df
+        )
+
+        trials_df = generate_trials_df(
+            recording=recording, imaging_df=imaging_df
+        )
+
+        trials_df = search_param_log_trials(
+            recording=recording,
+            trials_df=trials_df,
+            flexilims_session=flexilims_session,
+        )
+
+        if i == 0:
+            vs_df_all = vs_df
+            trials_df_all = trials_df
+        else:
+            vs_df_all = pd.concat([vs_df_all, vs_df], ignore_index=True)
+            trials_df_all = pd.concat([trials_df_all, trials_df], ignore_index=True)
     print(f"Finished concatenating vs_df and trials_df")
 
     return vs_df_all, trials_df_all
