9b9a0a10cdf60432013eed93b197074ed53aab9a
Antonin Blot <antonin.blot@crick.ac.uk>
2023-08-13 14:07:38 +0100

    [refactor] change minicam to use slurm_it
    Instead of redificining a slurm function locally

--- cottage_analysis/preprocessing/minicam.py
+++ cottage_analysis/preprocessing/minicam.py
@@ -1,10 +1,10 @@
 """Function to preprocess minicam data."""
 
-
 from functools import partial
 from pathlib import Path
 import flexiznam as flz
 from flexiznam.schema import CameraData
+from znamutils import slurm_it
 from cottage_analysis.io_module import video
 from cottage_analysis.utilities import slurm_helper
 
@@ -38,18 +38,26 @@ def run_deinterleave(camera_ds, redo=False, use_slurm=True, dependency=None):
 
     print("Deinterleaving %s" % camera_ds.full_name)
     if use_slurm:
-        func = partial(
-            slurm_deinterleave,
+        slurm_folder = target_ds.path_full
+        slurm_folder.mkdir(parents=True, exist_ok=True)
+        job_id = deinterleave(
+            camera_ds.id,
+            project_id=camera_ds.project_id,
+            use_slurm=use_slurm,
             slurm_folder=target_ds.path_full,
-            dependency=dependency,
         )
     else:
-        func = deinterleave
-    job_id, slurm_folder = func(camera_ds.id, project_id=camera_ds.project_id)
-
+        slurm_folder = deinterleave(camera_ds.id, project_id=camera_ds.project_id)
+        job_id = None
     return job_id, slurm_folder
 
 
+@slurm_it(
+    conda_env="cottage_analysis",
+    slurm_options=dict(mem="8G", time="24:00:00"),
+    module_list=["FFmpeg"],
+    from_imports={"cottage_analysis.preprocessing.minicam": "deinterleave"},
+)
 def deinterleave(camera_ds_id, project_id):
     """Deinterleave a camera dataset.
 
@@ -82,32 +90,5 @@ def deinterleave(camera_ds_id, project_id):
         intrinsic_calibration=None,
     )
     camera_ds.path_full / camera_ds.extra_attributes["metadata_file"]
-    target_ds.update_flexilims(conflicts="overwrite")
-    return None, target_ds.path_full
-
-
-def slurm_deinterleave(camera_ds_id, project_id, slurm_folder, dependency=None):
-    slurm_folder = Path(slurm_folder)
-    python_script = slurm_folder / "deinterleave.py"
-    flm_sess = flz.get_flexilims_session(project_id=project_id)
-    camera_ds = flz.Dataset.from_flexilims(id=camera_ds_id, flexilims_session=flm_sess)
-
-    slurm_helper.python_script_single_func(
-        target_file=python_script,
-        function_name="cottage_analysis.preprocessing.minicam.deinterleave",
-        arguments=dict(camera_ds_id=camera_ds_id, project_id=camera_ds.project_id),
-        imports="cottage_analysis.preprocessing.minicam",
-    )
-    slurm_helper.create_slurm_sbatch(
-        target_folder=slurm_folder,
-        script_name="deinterleave.sh",
-        python_script=python_script,
-        conda_env="cottage_analysis",
-        slurm_options=dict(mem="8G", time="24:00:00"),
-        module_list=["FFmpeg"],
-    )
-
-    job_id = slurm_helper.run_slurm_batch(
-        slurm_folder / "deinterleave.sh", job_dependency=dependency
-    )
-    return job_id, slurm_folder
+    target_ds.update_flexilims(mode="overwrite")
+    return target_ds.path_full
