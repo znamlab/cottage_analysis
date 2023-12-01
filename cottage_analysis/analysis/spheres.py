ab8345559bf531b4e98b7957141df9fa77202020
yiran25 <yiran.he@crick.ac.uk>
2023-12-01 14:27:34 +0000

    [bugfix] Tolerate the problem with the timing of last imaging frame being later than last found monitor frame

--- cottage_analysis/analysis/spheres.py
+++ cottage_analysis/analysis/spheres.py
@@ -122,8 +122,8 @@ def regenerate_frames(
     )
     trial_index = np.clip(trial_index, 0, len(trials_df) - 1)
     frame_indices = find_valid_frames(frame_times, trials_df, verbose=verbose)
-    if frame_times[-1] > mouse_pos_time[-1]:
-        frame_times[-1] = frame_times[-2]
+    # If the imaging frame is after the last found monitor frame, use the time for the last imaging frame time that's before the last found monitor frame
+    frame_times[np.where(frame_times>mouse_pos_time[-1])[0]] = frame_times[np.where(frame_times<=mouse_pos_time[-1])[0][-1]]
     mouse_position = mouse_pos_cm[mouse_pos_time.searchsorted(frame_times)]
 
     # now process the valid frames
