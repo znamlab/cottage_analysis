1bf3ad6a652526231d5adde8a7f467d2eccc06d3
Antonin Blot <antonin.blot@crick.ac.uk>
2023-12-14 15:13:32 +0000

    [feature] add label video on dlc module

--- cottage_analysis/dlc/label_video.py
+++ cottage_analysis/dlc/label_video.py
@@ -2,10 +2,16 @@ from pathlib import Path
 import deeplabcut
 
 model = "headfixed_track_eye"
-video = "/nem/"
+data_fold = Path("/camp/lab/znamenskiyp/home/shared/projects/blota_onix_pilote/")
+data_fold /= "BRYA142.5d/S20231024/R140945_wehrcam_headmounted/"
+video = data_fold / "eye_camera_deinterleaved_0/eye_camera_deinterleaved.mp4"
 
 config_path = Path("/nemo/lab/znamenskiyp/home/shared/projects/DLC_models/")
 config_path /= f"{model}/config.yaml"
 
-
-deeplabcut.create_labeled_video(config_path, video, videotype=".mp4")
+deeplabcut.create_labeled_video(
+    config_path,
+    str(video),
+    videotype=".mp4",
+    destfolder=str(data_fold / "eye_camera_deinterleaved_0_dlc_tracking_cropped_0"),
+)
