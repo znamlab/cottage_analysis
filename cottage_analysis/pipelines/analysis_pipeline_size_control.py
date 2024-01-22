6946dd95b00a094a9c72b4dc4c5d2077cf636388
yiran25 <yiran.he@crick.ac.uk>
2024-01-22 18:12:54 +0000

    [feature] update analysis pipeline for size control

---
+++ cottage_analysis/pipelines/analysis_pipeline_size_control.py
@@ -0,0 +1,134 @@
+import os
+import numpy as np
+import pandas as pd
+import defopt
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pickle
+from tqdm import tqdm
+
+import flexiznam as flz
+from cottage_analysis.preprocessing import synchronisation
+from cottage_analysis.analysis import (
+    spheres,
+    find_depth_neurons,
+    fit_gaussian_blob,
+    common_utils,
+    size_control,
+)
+from cottage_analysis.plotting import basic_vis_plots, sta_plots
+
+from cottage_analysis.pipelines import pipeline_utils
+
+# TODO: add decoder
+# TODO: separate steps
+
+
+def main(
+    project, session_name, conflicts="skip", photodiode_protocol=5,
+):
+    """
+    Main function to analyze a session.
+
+    Args:
+        project(str): project name
+        session_name(str): {Mouse}_{Session}
+        conflicts(str): "skip", "append", or "overwrite"
+        photodiode_protocol(int): 2 or 5.
+    """
+    print(
+        f"------------------------------- \n \
+        Start analysing {session_name}   \n \
+        -------------------------------"
+    )
+
+    flexilims_session = flz.get_flexilims_session(project)
+
+    neurons_ds = pipeline_utils.create_neurons_ds(
+        session_name=session_name,
+        flexilims_session=flexilims_session,
+        project=project,
+        conflicts=conflicts,
+    )
+    if (neurons_ds.get_flexilims_entry() is not None) and conflicts == "skip":
+        print(f"Session {session_name} already processed... reading saved data...")
+    else:
+        # Synchronisation
+        print("---Start synchronisation...---")
+        vs_df_all, trials_df_all = size_control.sync_all_recordings(
+            session_name=session_name,
+            flexilims_session=flexilims_session,
+            project=project,
+            filter_datasets={"anatomical_only": 3},
+            recording_type="two_photon",
+            protocol_base="SizeControl",
+            photodiode_protocol=photodiode_protocol,
+            return_volumes=True,
+        )
+
+        # Find depth neurons and fit preferred depth
+        print("---Start finding depth neurons...---")
+        print("Find depth neurons...")
+        neurons_df, neurons_ds = find_depth_neurons.find_depth_neurons(
+            trials_df=trials_df_all,
+            neurons_ds=neurons_ds,
+            rs_thr=0.2,
+            alpha=0.05,
+        )
+
+        # print("Fit preferred depth...")
+        # # Find preferred depth of closed loop with all data
+        # neurons_df, neurons_ds = find_depth_neurons.fit_preferred_depth(
+        #     trials_df=trials_df_all,
+        #     neurons_df=neurons_df,
+        #     neurons_ds=neurons_ds,
+        #     closed_loop=1,
+        #     choose_trials=None,
+        #     depth_min=0.02,
+        #     depth_max=20,
+        #     niter=10,
+        #     min_sigma=0.5,
+        #     k_folds=1,
+        # )
+
+        # # Find r-squared of k-fold cross validation
+        # neurons_df, neurons_ds = find_depth_neurons.fit_preferred_depth(
+        #     trials_df=trials_df_all,
+        #     neurons_df=neurons_df,
+        #     neurons_ds=neurons_ds,
+        #     closed_loop=1,
+        #     choose_trials=None,
+        #     depth_min=0.02,
+        #     depth_max=20,
+        #     niter=10,
+        #     min_sigma=0.5,
+        #     k_folds=5,
+        # )
+        
+        # Fit preferred depth for different sizes
+        for isize, size in enumerate(np.sort(trials_df_all["size"].unique())):
+            neurons_df_temp, neurons_ds = find_depth_neurons.fit_preferred_depth(
+                trials_df=trials_df_all[trials_df_all["size"] == size],
+                neurons_df=neurons_df,
+                neurons_ds=neurons_ds,
+                closed_loop=1,
+                choose_trials=None,
+                depth_min=0.02,
+                depth_max=20,
+                niter=10,
+                min_sigma=0.5,
+                k_folds=1,
+            )
+            neurons_df[f"preferred_depth_size{int(size)}"] = neurons_df_temp["preferred_depth_closedloop"]
+            neurons_df[f"depth_tuning_popt_size{int(size)}"] = neurons_df_temp["depth_tuning_popt_closedloop"]
+        
+        # Save neurons_df
+        # neurons_df.to_pickle(neurons_ds.path_full)
+        # neurons_ds.update_flexilims(mode="update")
+        
+    return neurons_df
+        
+
+
+if __name__ == "__main__":
+    defopt.run(main)
