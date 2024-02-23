7d603da2eb8c3036d8ca23d40c36f6f13551e880
yiran25 <yiran.he@crick.ac.uk>
2024-02-23 17:17:34 +0000

    [feature] change train test split for depth decoder to trial based. Add normalization based on true class for confusion matrix plot

--- cottage_analysis/analysis/population_depth_decoder.py
+++ cottage_analysis/analysis/population_depth_decoder.py
@@ -26,6 +26,7 @@ def rolling_average_2d(arr, window, axis=0):
     # calculate rolling average along an axis for 2d array
     return pd.DataFrame(arr).rolling(window=window, axis=axis, min_periods=1).mean().values
 
+
 def downsample_2d(arr, factor, mode="average"):
     # downsample 2d array by factor along axis 0
     end = int(factor * int(arr.shape[0] / factor))
@@ -36,11 +37,23 @@ def downsample_2d(arr, factor, mode="average"):
         arr_mean = arr[::factor, :]
     return arr_mean
 
-def fit_svm_classifier(X, y, class_labels, test_size=0.2, random_state=42, kernel="linear", Cs=[0.1, 1, 10], gammas=[1, 0.1, 0.01]):
-    # split data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=test_size, random_state=random_state)
-        
+
+def downsample_dff(
+                  trials_df, 
+                  rolling_window=0.5, 
+                  frame_rate=15, 
+                  downsample_window=0.5, 
+                  ):
+    trials_df["dff_stim_rolling"] = trials_df["dff_stim"].apply(lambda x: rolling_average_2d(x, window=int(rolling_window*frame_rate), axis=0)) 
+    trials_df["dff_stim_downsample"] = trials_df["dff_stim_rolling"].apply(lambda x: downsample_2d(x, factor=int(downsample_window*frame_rate), mode="average"))
+    depth_list = np.sort(trials_df.depth.unique())
+    trials_df["depth_label"] = trials_df["depth"].apply(lambda x: np.where(depth_list==x)[0])
+    trials_df["depth_labels"] = trials_df.apply(lambda x: np.repeat(x["depth_label"], x["dff_stim_downsample"].shape[0], axis=0), axis=1)
+    
+    return trials_df
+
+
+def fit_svm_classifier(X_train, y_train, X_test, y_test, X_val, y_val, class_labels, kernel="linear", Cs=[0.1, 1, 10], gammas=[1, 0.1, 0.01]):       
     # tune hyperparameters:
     best_acc = 0
     best_params = {"C": Cs[0], "gamma": gammas[0]}
@@ -51,10 +64,12 @@ def fit_svm_classifier(X, y, class_labels, test_size=0.2, random_state=42, kerne
             clf.fit(X_train, y_train)
             y_pred = clf.predict(X_val)
             acc = accuracy_score(y_val, y_pred)
+            print(f"Accuracy for C{C}: {acc}")
 
             if acc > best_acc:
                 best_acc = acc
                 best_params = {"C": C}
+        print(f"Best C: {best_params['C']}")
     else:
         for C in Cs:
             for gamma in gammas:
@@ -67,6 +82,7 @@ def fit_svm_classifier(X, y, class_labels, test_size=0.2, random_state=42, kerne
                 if acc > best_acc:
                     best_acc = acc
                     best_params = {"C": C, "gamma": gamma}
+        print(f"Best C: {best_params['C']}, Best gamma: {best_params['gamma']}")
     
     # Train the final classifier on the best hyperparameters
     if kernel == "linear":
@@ -74,24 +90,74 @@ def fit_svm_classifier(X, y, class_labels, test_size=0.2, random_state=42, kerne
     else:
         clf = SVC(C=best_params["C"], gamma=best_params["gamma"], kernel=kernel)
     clf.fit(X_train, y_train)
+    
     # Evaluate the accuracy of the classifier
     y_pred = clf.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     conmat = confusion_matrix(y_test, y_pred, labels=class_labels)
 
-    return acc, conmat
+    return acc, conmat, best_params
 
 
-def depth_decoder(trials_df, rolling_window=0.5, frame_rate=15, downsample_window=0.5, test_size=0.2, random_state=42, kernel="linear", Cs=[0.1, 1, 10]):
-    trials_df["dff_stim_rolling"] = trials_df["dff_stim"].apply(lambda x: rolling_average_2d(x, window=int(rolling_window*frame_rate), axis=0)) 
-    trials_df["dff_stim_downsample"] = trials_df["dff_stim_rolling"].apply(lambda x: downsample_2d(x, factor=int(downsample_window*frame_rate), mode="average"))
+def depth_decoder(trials_df, 
+                  flexilims_session, 
+                  session_name, 
+                  rolling_window=0.5, 
+                  frame_rate=15, 
+                  downsample_window=0.5, 
+                  test_size=0.2, 
+                  random_state=42, 
+                  kernel="linear", 
+                  Cs=[0.1, 1, 10], 
+                  gammas=[1, 0.1, 0.01]):
+    # add iscell
+    suite2p_ds = flz.get_datasets(
+        flexilims_session=flexilims_session,
+        origin_name=session_name,
+        dataset_type="suite2p_rois",
+        filter_datasets={"anatomical_only": 3},
+        allow_multiple=False,
+        return_dataseries=False,
+    )
+    iscell = np.load(suite2p_ds.path_full / "plane0" / "iscell.npy", allow_pickle=True)[:,0]
+
+    # process dff and trials_df
+    trials_df = downsample_dff(trials_df,
+                               rolling_window=rolling_window,
+                               frame_rate=frame_rate,
+                               downsample_window=downsample_window, )
+    
+    # train test val split
     depth_list = np.sort(trials_df.depth.unique())
-    trials_df["depth_label"] = trials_df["depth"].apply(lambda x: np.where(depth_list==x)[0])
-    trials_df["depth_label"] = trials_df.apply(lambda x: np.repeat(x["depth_label"], x["dff_stim_downsample"].shape[0], axis=0), axis=1)
+    depth_label = np.hstack(trials_df["depth_label"].values)
+    stratified_kfold = StratifiedKFold(
+        n_splits=int(1/test_size), shuffle=True, random_state=random_state
+    )
+    # Train test split
+    for fold, (train_index, test_index) in enumerate(stratified_kfold.split(np.arange(len(depth_label)), depth_label)):
+        depth_label_train = depth_label[train_index]
+        dff_test = np.vstack(trials_df.iloc[test_index]["dff_stim_downsample"].values)
+        depth_test = np.hstack(trials_df.iloc[test_index]["depth_labels"].values)
+        break # to only take the first fold
 
-    downsampled_trace = np.vstack(trials_df["dff_stim_downsample"].values)
-    depth_trace = np.hstack(trials_df["depth_label"].values)
+    # Train val split
+    for fold, (train_index, val_index) in enumerate(stratified_kfold.split(np.arange(len(depth_label_train)), depth_label_train)):
+        dff_train = np.vstack(trials_df.iloc[train_index]["dff_stim_downsample"].values)
+        dff_val = np.vstack(trials_df.iloc[val_index]["dff_stim_downsample"].values)
+        depth_train = np.hstack(trials_df.iloc[train_index]["depth_labels"].values)
+        depth_val = np.hstack(trials_df.iloc[val_index]["depth_labels"].values)
+        break
     
-    acc, conmat = fit_svm_classifier(X=downsampled_trace, y=depth_trace, class_labels=np.arange(len(depth_list)), test_size=test_size, random_state=random_state, kernel=kernel, Cs=Cs)
+    acc, conmat, best_params = fit_svm_classifier(
+                                     X_train = dff_train, 
+                                     y_train = depth_train, 
+                                     X_test = dff_test,
+                                     y_test = depth_test,
+                                     X_val = dff_val,
+                                     y_val = depth_val,
+                                     class_labels=np.arange(len(depth_list)), 
+                                     kernel=kernel,
+                                     Cs=Cs, 
+                                     gammas=gammas)
     
-    return acc, conmat
+    return acc, conmat, best_params
--- cottage_analysis/plotting/depth_decoder_plots.py
+++ cottage_analysis/plotting/depth_decoder_plots.py
@@ -1,19 +1,29 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import itertools
 
-def plot_confusion_matrix(conmat, acc, fontsize_dict):
+def plot_confusion_matrix(conmat, acc, normalize, fontsize_dict):
+    # Normalize confusion matrix with true labels
+    if normalize:
+        conmat = conmat / conmat.sum(axis=1)[:, np.newaxis]
+        
     plt.imshow(conmat, interpolation="nearest", cmap="Blues")
     fmt = "d"
     thresh = conmat.max() / 2.0
     for i, j in itertools.product(range(conmat.shape[0]), range(conmat.shape[1])):
+        display = conmat[i, j]
+        if display >=1:
+            display = str(int(display))
+        else:
+            display = f"{display:.2f}"
         plt.text(
             j,
             i,
-            str(int(conmat[i, j])),
+            display,
             horizontalalignment="center",
             color="white" if conmat[i, j] > thresh else "black",
             fontsize=fontsize_dict["text"],
         )
-    plt.xlabel("Predicted depth class", fontsize=fontsize_dict["xlabel"])
-    plt.ylabel("True depth class", fontsize=fontsize_dict["ylabel"])
+    plt.xlabel("Predicted depth class", fontsize=fontsize_dict["label"])
+    plt.ylabel("True depth class", fontsize=fontsize_dict["label"])
     plt.title(f"Accuracy {acc:.2f}")
