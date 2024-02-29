d8f88ca2ef450b6dfde05cd5fc7b89d4373d7b59
yiran25 <yiran.he@crick.ac.uk>
2024-02-29 17:48:17 +0000

    [minor] update analysis pipeline

--- cottage_analysis/pipelines/analysis_pipeline.py
+++ cottage_analysis/pipelines/analysis_pipeline.py
@@ -188,81 +188,93 @@ def main(
                 neurons_df.at[i, f"rf_coef{sfx}"] = coef[:, :, i]
                 neurons_df.at[i, f"rf_coef_ipsi{sfx}"] = coef_ipsi[:, :, i]
                 neurons_df.at[i, f"rf_rsq{sfx}"] = r2[i, :]
-                neurons_df.at[i, f"rf_coef_ipsi{sfx}"] = r2_ipsi[i, :]
+                neurons_df.at[i, f"rf_rsq_ipsi{sfx}"] = r2_ipsi[i, :]
                 neurons_df.at[i, f"rf_reg_xy{sfx}"] = best_reg_xys[i]
                 neurons_df.at[i, f"rf_reg_depth{sfx}"] = best_reg_depths[i]
 
         # Save neurons_df
         neurons_df.to_pickle(neurons_ds.path_full)
 
-        # Update neurons_ds on flexilims
-        neurons_ds.update_flexilims(mode="update")
-
-        # Fit gaussian blob to neuronal activity
-        print("---Start fitting 2D gaussian blob...---")
-        outputs = []
-        common_params = dict(
-            rs_thr=0.01,
-            param_range={"rs_min": 0.005, "rs_max": 5, "of_min": 0.03, "of_max": 3000},
-            niter=5,
-            min_sigma=0.25,
-        )
-                
-        to_do = [
-            ("gaussian_2d", None, 1),
-            ("gaussian_2d", "even", 1),
-            ("gaussian_additive", None, 1),
-            ("gaussian_OF", None, 1),
-            ("gaussian_2d", None, 5),
-            ("gaussian_additive", None, 5),
-            ("gaussian_OF", None, 5),
-        ]
-
-        for model, trials, k_folds in to_do:
-            name = f"{session_name}_{model}"
-            if trials is not None:
-                name += "_crossval"
-            name += f"_k{k_folds}"  
-            print(f"Fitting {model}")
-            out = pipeline_utils.load_and_fit(
-                project,
-                session_name,
-                photodiode_protocol,
-                model=model,
-                choose_trials=trials,
-                use_slurm=use_slurm,
-                slurm_folder=slurm_folder,
-                scripts_name=name,
-                k_folds=k_folds,
-                **common_params,
-            )
-            outputs.append(out)
-
+        # # Update neurons_ds on flexilims
+        # neurons_ds.update_flexilims(mode="update")
+        
         # Merge fit dataframes
-        job_dependency = outputs if use_slurm else None
         out = pipeline_utils.merge_fit_dataframes(
             project,
             session_name,
-            use_slurm=use_slurm,
+            use_slurm=0,
             slurm_folder=slurm_folder,
-            job_dependency=job_dependency,
+            job_dependency=None,
             scripts_name=f"{session_name}_merge_fit_dataframes",
         )
         
         print("---Analysis finished. Neurons_df saved.---")
 
-        # Plot basic plots
-        print("---Start basic vis plotting...---")
-        pipeline_utils.run_basic_plots(
-            project,
-            session_name,
-            photodiode_protocol,
-            use_slurm=use_slurm,
-            slurm_folder=slurm_folder,
-            job_dependency=job_dependency,
-            scripts_name=f"{session_name}_basic_vis_plots",
-        )
-        print("---Plotting finished. ---")
+        # # Fit gaussian blob to neuronal activity
+        # print("---Start fitting 2D gaussian blob...---")
+        # outputs = []
+        # common_params = dict(
+        #     rs_thr=0.01,
+        #     param_range={"rs_min": 0.005, "rs_max": 5, "of_min": 0.03, "of_max": 3000},
+        #     niter=5,
+        #     min_sigma=0.25,
+        # )
+                
+        # to_do = [
+        #     ("gaussian_2d", None, 1),
+        #     ("gaussian_2d", "even", 1),
+        #     ("gaussian_additive", None, 1),
+        #     ("gaussian_OF", None, 1),
+        #     ("gaussian_2d", None, 5),
+        #     ("gaussian_additive", None, 5),
+        #     ("gaussian_OF", None, 5),
+        # ]
+
+        # for model, trials, k_folds in to_do:
+        #     name = f"{session_name}_{model}"
+        #     if trials is not None:
+        #         name += "_crossval"
+        #     name += f"_k{k_folds}"  
+        #     print(f"Fitting {model}")
+        #     out = pipeline_utils.load_and_fit(
+        #         project,
+        #         session_name,
+        #         photodiode_protocol,
+        #         model=model,
+        #         choose_trials=trials,
+        #         use_slurm=use_slurm,
+        #         slurm_folder=slurm_folder,
+        #         scripts_name=name,
+        #         k_folds=k_folds,
+        #         **common_params,
+        #     )
+        #     outputs.append(out)
+
+        # # Merge fit dataframes
+        # job_dependency = outputs if use_slurm else None
+        # out = pipeline_utils.merge_fit_dataframes(
+        #     project,
+        #     session_name,
+        #     use_slurm=use_slurm,
+        #     slurm_folder=slurm_folder,
+        #     job_dependency=job_dependency,
+        #     scripts_name=f"{session_name}_merge_fit_dataframes",
+        # )
+        
+        # print("---Analysis finished. Neurons_df saved.---")
+
+        # # Plot basic plots
+        # print("---Start basic vis plotting...---")
+        # pipeline_utils.run_basic_plots(
+        #     project,
+        #     session_name,
+        #     photodiode_protocol,
+        #     use_slurm=use_slurm,
+        #     slurm_folder=slurm_folder,
+        #     job_dependency=job_dependency,
+        #     scripts_name=f"{session_name}_basic_vis_plots",
+        # )
+        # print("---Plotting finished. ---")
 
 
 if __name__ == "__main__":
