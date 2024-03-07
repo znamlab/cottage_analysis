04f2d91c8bc5009ca316db064158c82ece949326
yiran25 <yiran.he@crick.ac.uk>
2024-03-07 21:26:14 +0000

    [bugfix] fix train/val set indexing bug

--- cottage_analysis/analysis/population_depth_decoder.py
+++ cottage_analysis/analysis/population_depth_decoder.py
@@ -16,13 +16,49 @@ from tqdm import tqdm
 import scipy
 import itertools
 
-from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedShuffleSplit
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 import flexiznam as flz
 from cottage_analysis.analysis import spheres, common_utils, find_depth_neurons
 from cottage_analysis.pipelines import pipeline_utils
+
+
+def stratified_shuffle_split(X,y, test_size=0.2, random_state=42):
+    # Ensure the data is in a numpy array for easier indexing
+    X = np.array(X)
+    y = np.array(y)
+    
+    # Determine unique classes and their distribution
+    classes, y_indices = np.unique(y, return_inverse=True)
+    class_counts = np.bincount(y_indices)
+    
+    # Calculate the number of samples per class in the test set
+    test_counts = np.ceil(class_counts * test_size).astype(int)
+    train_counts = class_counts - test_counts
+    
+    # Initialize lists to hold the indices for the split
+    train_indices = []
+    test_indices = []
+    
+    # For each class, shuffle and split the data
+    for class_index, class_count in enumerate(class_counts):
+        class_indices = np.where(y == classes[class_index])[0]
+        np.random.shuffle(class_indices)
+        
+        class_test_indices = class_indices[:test_counts[class_index]]
+        class_train_indices = class_indices[test_counts[class_index]:]
+        
+        train_indices.extend(class_train_indices)
+        test_indices.extend(class_test_indices)
+
+    
+    # # Shuffle the final indices to mix classes
+    # np.random.shuffle(train_indices)
+    # np.random.shuffle(test_indices)
+    
+    return train_indices, test_indices
 
 
 def rolling_average_2d(arr, window, axis=0):
@@ -128,10 +164,44 @@ def fit_svm_classifier(
     return acc, conmat, best_params
 
 
+def split_train_test_val(trials_df, test_size=0.2, random_state=42, trial_average=False):
+    depth_list = np.sort(trials_df.depth.unique())
+    trials_df = trials_df[:-(len(trials_df)%len(depth_list))] 
+    if trial_average:
+        dff_col = "trial_mean_dff"
+        depth_col = "depth_label"
+    else:
+        dff_col = "dff_stim_downsample"
+        depth_col = "depth_labels"
+    # train test val split
+    depth_label = np.hstack(trials_df["depth_label"].values)
+
+    # Train test split
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=random_state)
+    for fold, (train_index_temp, test_index) in enumerate(sss.split(np.arange(len(depth_label)), depth_label)):
+        depth_label_train = depth_label[train_index_temp]
+        dff_test = np.vstack(trials_df.iloc[test_index][dff_col].values)
+        depth_test = np.hstack(trials_df.iloc[test_index][depth_col].values)
+
+    # Train val split
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size/(1-test_size), random_state=random_state)
+    for fold, (train_index, val_index) in enumerate(sss.split(np.arange(len(depth_label_train)), depth_label_train)):
+        train_index = train_index_temp[train_index]
+        val_index = train_index_temp[val_index]
+        dff_train = np.vstack(trials_df.iloc[train_index][dff_col].values)
+        dff_val = np.vstack(trials_df.iloc[val_index][dff_col].values)
+        depth_train = np.hstack(trials_df.iloc[train_index][depth_col].values)
+        depth_val = np.hstack(trials_df.iloc[val_index][depth_col].values)
+        
+    return dff_train, dff_val, dff_test, depth_train, depth_val, depth_test
+    
+
 def depth_decoder(
     trials_df,
     flexilims_session,
     session_name,
+    closed_loop=1,
+    trial_average=False,
     rolling_window=0.5,
     frame_rate=15,
     downsample_window=0.5,
@@ -141,6 +211,9 @@ def depth_decoder(
     Cs=[0.1, 1, 10],
     gammas=[1, 0.1, 0.01],
 ):
+    # choose closedloop or openloop
+    trials_df = trials_df[trials_df.closed_loop == closed_loop]
+    
     # add iscell
     suite2p_ds = flz.get_datasets(
         flexilims_session=flexilims_session,
@@ -161,31 +234,24 @@ def depth_decoder(
         frame_rate=frame_rate,
         downsample_window=downsample_window,
     )
+    trials_df = find_depth_neurons.trial_average_dff(
+        trials_df,
+        rs_thr_min=None,
+        rs_thr_max=None,
+        still_only=False,
+        still_time=0,
+        frame_rate=15,
+        closed_loop=closed_loop,
+    )
+    depth_list = np.sort(trials_df.depth.unique())
 
     # train test val split
-    depth_list = np.sort(trials_df.depth.unique())
-    depth_label = np.hstack(trials_df["depth_label"].values)
-    stratified_kfold = StratifiedKFold(
-        n_splits=int(1 / test_size), shuffle=True, random_state=random_state
-    )
-    # Train test split
-    for fold, (train_index, test_index) in enumerate(
-        stratified_kfold.split(np.arange(len(depth_label)), depth_label)
-    ):
-        depth_label_train = depth_label[train_index]
-        dff_test = np.vstack(trials_df.iloc[test_index]["dff_stim_downsample"].values)
-        depth_test = np.hstack(trials_df.iloc[test_index]["depth_labels"].values)
-        break  # to only take the first fold
 
-    # Train val split
-    for fold, (train_index, val_index) in enumerate(
-        stratified_kfold.split(np.arange(len(depth_label_train)), depth_label_train)
-    ):
-        dff_train = np.vstack(trials_df.iloc[train_index]["dff_stim_downsample"].values)
-        dff_val = np.vstack(trials_df.iloc[val_index]["dff_stim_downsample"].values)
-        depth_train = np.hstack(trials_df.iloc[train_index]["depth_labels"].values)
-        depth_val = np.hstack(trials_df.iloc[val_index]["depth_labels"].values)
-        break
+    dff_train, dff_val, dff_test, depth_train, depth_val, depth_test = split_train_test_val(
+        trials_df=trials_df, 
+        test_size=0.2, 
+        random_state=42, 
+        trial_average=trial_average)
 
     acc, conmat, best_params = fit_svm_classifier(
         X_train=dff_train,
