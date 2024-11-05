4da12f07b9e2c97a1174e0ba8242dde28ee5d7d9
yiran25 <yiran.he@crick.ac.uk>
2024-11-05 16:10:41 +0000

    [rename] rename summary_analysis

--- __init__ .py
+++ __init__ .py
@@ -1,1 +1,1 @@
-from cottage_analysis import analysis, dlc, ephys, eye_tracking, imaging, io_module, pipelines, plotting, preprocessing, population_analysis
+from cottage_analysis import analysis, dlc, ephys, eye_tracking, imaging, io_module, pipelines, plotting, preprocessing, summary_analysis
