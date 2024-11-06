b07d9a6b35d66b40f7f61c8f1dd231eafa995e62
yiran25 <yiran.he@crick.ac.uk>
2024-11-06 17:17:19 +0000

    [minor] clean SFTF pipeline

--- cottage_analysis/pipelines/depth_SFTF.py
+++ cottage_analysis/pipelines/depth_SFTF.py
@@ -29,18 +29,6 @@ def main(project, session_name, conflicts="skip", photodiode_protocol=5):
     flexilims_session = flz.get_flexilims_session(project)
     # Synchronisation
     print("---Start synchronisation...---")
-    # # Sync sphere recordings
-    # print("---Syncing sphere recordings...---")
-    # vs_df_all, trials_df_all = spheres.sync_all_recordings(
-    #     session_name=session_name,
-    #     flexilims_session=flexilims_session,
-    #     project=project,
-    #     filter_datasets={"anatomical_only": 3},
-    #     recording_type="two_photon",
-    #     protocol_base="SpheresPermTubeReward",
-    #     photodiode_protocol=photodiode_protocol,
-    #     return_volumes=True,
-    # )
     # Sync SFTF recordings
     print("---Syncing SFTF recordings...---")
     trials_df_all_sftf, dff_mean_all_sftf = gratings.analyze_grating_responses(
@@ -58,27 +46,12 @@ def main(project, session_name, conflicts="skip", photodiode_protocol=5):
         conflicts=conflicts,
     )
 
-    # if (neurons_ds.get_flexilims_entry() is not None) and conflicts == "skip":
-    #     print("Session already analyzed.")
-    #     neurons_df = pd.read_pickle(neurons_ds.path_full)
-
     # Anlyze SFTF responses
     print("---Fitting SFTF responses...---")
     neurons_df_sftf = fit_gaussian_blob.fit_sftf_tuning(
         trials_df=trials_df_all_sftf, niter=10, min_sigma=0.25
     )
     neurons_df_sftf.to_pickle(neurons_ds.path_full.parent / "neurons_df_sftf.pickle")
-
-    # # Analyze depth responses
-    # # Find depth neurons and fit preferred depth
-    # print("---Start finding depth neurons...---")
-    # print("Find depth neurons...")
-    # neurons_df, neurons_ds = find_depth_neurons.find_depth_neurons(
-    #     trials_df=trials_df_all,
-    #     neurons_ds=neurons_ds,
-    #     rs_thr=0.2,
-    #     alpha=0.05,
-    # )
 
     # # Visualize all neurons
     # print("---Start visualisation---")
