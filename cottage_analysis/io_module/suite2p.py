b181e73515745137d35a874cc50773ae55a73701
Caroline Cypranowska <cypranowska@gmail.com>
2025-02-10 07:05:41 +0000

    [feature] add iscell.npy from combined if directory exists

--- cottage_analysis/io_module/suite2p.py
+++ cottage_analysis/io_module/suite2p.py
@@ -13,12 +13,17 @@ def load_is_cell(suite2p_path: Path) -> np.ndarray:
         iscell: Array of booleans indicating whether each ROI is a cell.
     """
     suite2p_path = Path(suite2p_path)
-    plane_idx = 0
-    folder = suite2p_path / f"plane{plane_idx}"
     is_cell = []
-    while folder.exists():
+
+    if suite2p_path.joinpath("combined").exists():
+        folder = suite2p_path.joinpath("combined")
         is_cell.append(np.load(folder / "iscell.npy"))
-        plane_idx += 1
+    else:
+        plane_idx = 0
         folder = suite2p_path / f"plane{plane_idx}"
+        while folder.exists():
+            is_cell.append(np.load(folder / "iscell.npy"))
+            plane_idx += 1
+            folder = suite2p_path / f"plane{plane_idx}"
 
     return np.vstack(is_cell)[:, 0].astype(bool)
