e878630a909898ae08537a4f05ac13c12dab1152
Caroline Cypranowska <cypranowska@gmail.com>
2025-07-27 12:16:19 +0100

    [minor] improving how y axes limits are set, returning the axes and the speed bin error

--- cottage_analysis/plotting/depth_decoder_plots.py
+++ cottage_analysis/plotting/depth_decoder_plots.py
@@ -391,17 +391,21 @@ def plot_decoder_err_by_speeds(
     )
 
     # add chance level error
-    axes[1].axhline(
-        np.nanmean(decoder_df[f"error_chance_{sfx}"]),
-        color=linecolor_chance,
-        linestyle="dotted",
-        linewidth=linewidth,
-        alpha=alpha_chance,
-    )
+    for ax in axes:
+        ax.axhline(
+            np.nanmean(decoder_df[f"error_chance_{sfx}"]),
+            color=linecolor_chance,
+            linestyle="dotted",
+            linewidth=linewidth,
+            alpha=alpha_chance,
+        )
 
     # set axis of the stationary plot
     axes[0].set_xlim([-0.5, 0.5])
-    ylim = axes[0].get_ylim()
+    ylim = [*axes[0].get_ylim()]
+    print(ylim)
+    if ylim[1] < np.nanmean(decoder_df[f"error_chance_{sfx}"]):
+        ylim[1] = np.nanmean(decoder_df[f"error_chance_{sfx}"]) * 1.1
     axes[0].set_ylim([0, ylim[1]])
     axes[0].set_xticks([0])
     axes[0].set_xticklabels(["stationary"], rotation=60)
@@ -428,7 +432,7 @@ def plot_decoder_err_by_speeds(
     axes[1].spines["right"].set_visible(False)
     axes[1].spines["top"].set_visible(False)
     axes[1].set_yticks([])
-    return err_speed_bins
+    return axes, err_speed_bins
 
 
 def plot_decoder_acc_by_speeds(
