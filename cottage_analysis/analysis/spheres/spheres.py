23ffe5a123c4485a9f009ac391594a3de99e07aa
Antonin Blot <antonin.blot@crick.ac.uk>
2025-11-20 18:22:31 +0000

    [feature] add MotorSpeed to trials_df
    To have not only in step per second but in cm/s

--- cottage_analysis/analysis/spheres/spheres.py
+++ cottage_analysis/analysis/spheres/spheres.py
@@ -278,7 +278,7 @@ def generate_trials_df(
         "acceleration_abs_max",
         "acceleration_ratio_max",
     ]
-    optional_columns = ["expected_optic_flow", "MotorSps"]
+    optional_columns = ["expected_optic_flow", "MotorSps", "MotorSpeed"]
     for column in optional_columns:
         if column in imaging_df.columns:
             columns_to_assign.append(column)
