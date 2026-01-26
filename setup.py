e80171b1444fd94f748a1e5e672a682b98357295
Antonin Blot <antonin.blot@crick.ac.uk>
2026-01-26 19:32:52 +0000

    [minor] remove flora's helper
    The useful function should be integrated in a lab repo

--- setup.py
+++ setup.py
@@ -34,6 +34,5 @@ setup(
         "fastparquet",
         "flexiznam @ git+ssh://git@github.com/znamlab/flexiznam.git",
         "znamutils @ git+ssh://git@github.com/znamlab/znamutils.git",
-        "floras_helpers @ git+ssh://git@github.com/takacsflora/floras-helpers.git",
     ],
 )
